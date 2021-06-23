4be135706b2b28eb71964bf5242a09defba3af2c
Tres Serio <treserio@hotmail.com>
2021-06-23 15:09:58 -0700

    woah betty 4.0

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,11 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - prints out the required text using put
+ * Return: int 0
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
