f9ab19e01767f48f9bd30d32a3e11c90e319dff3
Tres Serio <treserio@hotmail.com>
2021-06-23 19:50:25 -0700

    101.1 tried moving values

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-fwrite(str, 1, sizeof(str), stderr);
+char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+fwrite(str, sizeof(str), 1, stderr);
 return (1);
 }
