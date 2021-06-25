177dd6ef57bc7b2ceea611d8bdd81e66ef545124
Tres Serio <treserio@hotmail.com>
2021-06-25 12:42:09 -0700

    Task 1.0 attempt

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <time.h>
+/**
+* main - the last number of a rand number and give details
+* Description: betty is silly
+* Return: string with variable values previously evaluated
+*/
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	rem = n % 10;
+	if (rem  > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, rem);
+	} else if (rem == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, rem);
+	} else if (rem < 6)
+	{
+		printf("Last digit of %d is %d and is greater than 5", n, rem);
+	}
+	return (0);
+}
