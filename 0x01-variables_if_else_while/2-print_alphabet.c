9698de48f2c9f3a1a065d800beeaedfcf032cd65
Tres Serio <treserio@hotmail.com>
2021-06-25 13:30:31 -0700

    2.1 clrd betty

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 /**
  * main - write out the alphabet
- * 
+ *
  * Return: A to Z
  */
-int main(void) {
-	char alpha[]="abcdefghijklmnopqrstuvwxyz"
+int main(void)
+{
+	char alpha[] = "abcdefghijklmnopqrstuvwxyz"
+
 	int ltr
-	for(ltr = "a"; ltr <= "z"; ++ltr)
+
+	for (ltr = "a"; ltr <= "z"; ++ltr)
 	{
 		putchar(ltr);
 	}
