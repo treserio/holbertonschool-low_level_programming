fc99806ecf1897fc96ebc0a218390bd3dd6fc64f
Tres Serio <treserio@hotmail.com>
2021-06-25 12:59:45 -0700

    corrected < string and removed abs()

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,7 +13,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	rem = abs(n % 10);
+	rem = n % 10;
 	if (rem  > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, rem);
@@ -22,7 +22,7 @@ int main(void)
 		printf("Last digit of %d is %d and is 0\n", n, rem);
 	} else if (rem < 6)
 	{
-		printf("Last digit of %d is %d and is greater than 5", n, rem);
+		printf("Last digit of %d is %d and is less than 6 and not 0", n, rem);
 	}
 	return (0);
 }
