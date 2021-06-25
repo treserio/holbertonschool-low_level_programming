6a11f1bf07c84a0c4ef291cc93039b7598409956
Tres Serio <treserio@hotmail.com>
2021-06-25 13:01:16 -0700

    T1.4 added \n

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@ int main(void)
 		printf("Last digit of %d is %d and is 0\n", n, rem);
 	} else if (rem < 6)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0", n, rem);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, rem);
 	}
 	return (0);
 }
