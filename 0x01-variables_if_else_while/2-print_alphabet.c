cb4f6df91bb6f6995238fdd2c968e75d3443d79e
Tres Serio <treserio@hotmail.com>
2021-06-25 15:24:37 -0500

    Task 2.0

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - write out the alphabet
+ * 
+ * Return: A to Z
+ */
+int main(void) {
+	char alpha[]="abcdefghijklmnopqrstuvwxyz"
+	int ltr
+	for(ltr = "a"; ltr <= "z"; ++ltr)
+	{
+		putchar(ltr);
+	}
+}
