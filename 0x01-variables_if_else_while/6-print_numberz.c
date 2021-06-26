f42e29e5ea12968ce8f553e9ff075bbaabc902f4
Tres Serio <treserio@hotmail.com>
2021-06-25 20:07:57 -0700

    6.0 init

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print 0-9 + \n
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; ++i)
+	{
+		putchar(i);
+	}
+	putchar("\n");
+	return (0);
+}
