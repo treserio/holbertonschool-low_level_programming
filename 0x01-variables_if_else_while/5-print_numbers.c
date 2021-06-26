a84058b08ddd0a5602f0b5df0093e4b4ce5cfb92
Tres Serio <treserio@hotmail.com>
2021-06-25 20:02:52 -0700

    5.1 added return and betty fix

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 /**
  * main - print 0-9 + \n
- * Returns: 0
+ * Return: 0
  */
 int main(void)
 {
+	int i;
+
 	for (i = 0; i < 10; ++i)
 	{
 		printf("%d", i);
 	}
 	printf("\n");
+	return (0);
 }
