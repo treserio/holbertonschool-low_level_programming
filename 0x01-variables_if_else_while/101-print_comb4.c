90ad3299c115fd1abc5e4c88da1884700582dbb0
Tres Serio <treserio@hotmail.com>
2021-06-25 21:32:44 -0700

    101 init

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print all permiations of two digit numbers, except where they're the
+ * Description: same number [11, 22, 33]
+ * Return: 0
+ */
+int main(void)
+{
+	int hund, tens, ones;
+
+	for (hund = 48; hund <= 57; ++hund)
+	{
+		for (tens = hund + 1; tens <= 57; ++tens)
+		{
+			for (ones = tens + 1; ones <= 57; ++ones)
+			{
+				putchar(tens);
+				putchar(ones);
+				if (hund != 55)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
