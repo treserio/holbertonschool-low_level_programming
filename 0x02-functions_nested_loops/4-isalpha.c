73725eb7581fd3130eeb425d64b18cd1baa6d4fa
Tres Serio <treserio@hotmail.com>
2021-06-28 12:20:07 -0700

    Init 4.0

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+/**
+ * _islower - tells if a char is lower or upper a-z
+ * @input : an int passed to the function
+ * Return: 1 if c is lowercase else 0
+ */
+
+int _islower(char input)
+{
+	int check = (int)(input);
+
+	if (check > 97 && check < 123) || (check > 64 && check < 91)
+	{
+		return (1);
+	}
+	return (0);
+}
