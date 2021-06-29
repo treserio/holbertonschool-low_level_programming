d1eac5c2807172f510198c49ec73ae00d057e7ee
Tres Serio <treserio@hotmail.com>
2021-06-28 22:53:03 -0700

    100 init

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,55 @@
+#include "holberton.h"
+/**
+ * print_times_table - print out a times table to the _nth power
+ * Return: void
+ */
+void print_times_table(int _n)
+{
+	int x, y, result;
+
+	for (y = 0; y < _n; ++y)
+	{
+		for (x = 0; x < _n; ++x)
+		{
+			result = y * x;
+			tbl_out(x, result);
+		}
+		_putchar('\n');
+	}
+}
+/**
+ * tbl_out - print ", " where needed
+ * @inner: loop value, x
+ * @multi: result value, or any int
+ * Return: void
+ */
+void tbl_out(int inner, int multi)
+{
+	int tens = multi / 10;
+	int ones = multi % 10;
+
+	if (inner == 0)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+	} else if (multi < 10)
+	{
+		_putchar(' ');
+		_putchar(multi + '0');
+		if (inner < 9)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	} else
+	{
+		_putchar(tens + '0');
+		_putchar(ones + '0');
+		if (inner < 9)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+}
