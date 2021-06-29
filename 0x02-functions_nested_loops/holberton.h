8c08360b4bbe1f9436f1ed620c9101aa68d3cba9
Tres Serio <treserio@hotmail.com>
2021-06-28 19:10:47 -0700

    added all prototypes to holberton.h

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -6,6 +6,11 @@ void print_alphabet_x10(void);
 int _islower(char);
 int _isalpha(char);
 int print_sign(int);
-
-
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
 #endif
