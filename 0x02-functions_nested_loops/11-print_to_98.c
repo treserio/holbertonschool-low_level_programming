12da6806bdf428bb74a259624bcbc11e887b2b78
Tres Serio <treserio@hotmail.com>
2021-06-28 22:45:25 -0700

    11.1 betty and bugs

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,28 +1,31 @@
 #include "holberton.h"
 #include <stdlib.h>
+#include <stdio.h>
 /**
  * print_to_98 - print numbers followed by ', ' from input to 98
  * @input: starting point
  * Return: nothing
  */
-void add(int input)
+void print_to_98(int input)
 {
 	if (input < 98)
 	{
-		while(input < 99)
+		while (input < 98)
 		{
-			printf("%d", input);
+			printf("%d, ", input);
 			++input;
 		}
-	} else if (input = 98)
+		printf("98");
+	} else if (input == 98)
 	{
 		printf("98");
 	} else
 	{
-		while(input > 97)
+		while (input > 97)
 		{
-			printf(%d, input);
+			printf("%d, ", input);
 			--input;
 		}
+		printf("98");
 	}
 }
