c9ba7bd697dffd44472b17e2e5abbacffcf2f1fa
Tres Serio <treserio@hotmail.com>
2021-06-28 22:30:21 -0700

    10.2 betty

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,9 +1,11 @@
 #include "holberton.h"
 /**
  * add - add two numbers and return result
+ * @a: number to be added
+ * @b: number to be added
  * Return: result of a + b
  */
 int add(int a, int b)
 {
-    return ( a + b );
+	return (a + b);
 }
