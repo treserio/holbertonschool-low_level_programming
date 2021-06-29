3aeb257394ea24ac397c2a56c40963e11105e0ef
Tres Serio <treserio@hotmail.com>
2021-06-28 21:16:26 -0700

    9.5 fixed my _putchar error

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,9 +14,22 @@ void times_table(void)
 		for (x = 0; x < 10; ++x)
 		{
 			result = y * x;
-			_putchar(result + '0');
-			_putchar(',');
-			_putchar(' ');
+			if (result > 9)
+			{
+				int tens = result / 10;
+				int ones = result % 10;
+				_putchar(tens + '0');
+				_putchar(ones + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(result + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 		_putchar('\n');
 	}
