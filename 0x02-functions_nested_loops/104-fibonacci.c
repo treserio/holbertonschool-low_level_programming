7ac186c2548754e62cb4b68f3b1c9f9f878ff90f
Tres Serio <treserio@hotmail.com>
2021-06-29 13:49:45 -0700

    104.12 tested break method

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,34 +1,52 @@
 #include <stdlib.h>
 #include <stdio.h>
+#define bil 1000000000
 /**
  * main - write a list first 100 fibonacci nums
- *
+ * Description: 9,223,372,036,854,775,807 potential range for long, break apart
+ * after billions to left / right
  * Return: 0
  */
 int main(void)
 {
+	unsigned long a1 = 0, a2 = 1, b1, b2 = 2, result1, result2, chek = 0;
 	int cntr = 2;
-	unsigned long a, b, c;
 
-	a = 1;
-	b = 2;
-	printf("%lu, %lu, ", a, b);
-
+	printf("%ld, %ld, ", a2, b2);
 	while (cntr < 98)
 	{
-		c = a + b;
-		a = b;
-		b = c;
-		if (cntr < 97)
+		if (chek == 0)
+			chek = a2 / bil;
+		if (chek > 0)
 		{
-			printf("%lu, ", c);
-			++cntr;
+			if (a1 == 0)
+			{
+				printf("\n\nSwapped");
+				a1 = a2 / bil;
+				a2 = a2 % bil;
+				b1 = b2 / bil;
+				b2 = b2 % bil;
+				result1 = a1 + b1;
+				result2 = a2 + b2;
+				printf("%lu%lu, ", result1, result2);
+			}
+			result1 = (a1 + b1) + ((a2 + b2) / bil);
+			result2 = (a2 + b2) % bil;
+			printf("%lu%lu", result1, result2);
+			a1 = b1;
+			a2 = b2;
+			b1 = result1;
+			b2 = result2;
 		}
 		else
 		{
-			printf("%lu\n", c);
-			++cntr;
+			result2 = a2 + b2;
+			a2 = b2;
+			b2 = result2;
+			printf("%ld, ", result2);
 		}
+		++cntr;
 	}
+	printf("\n");
 	return (0);
 }
