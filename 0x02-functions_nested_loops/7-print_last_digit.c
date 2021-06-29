eb5730dd8d7986f66acbc65f9911ff264a7126c1
Tres Serio <treserio@hotmail.com>
2021-06-28 19:39:06 -0700

    7.0 init

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * print_last_digit - grab the last digit of a number
+ * @num: number to be checked
+ * Return: the last num of a number
+ */
+int print_last_digit(int num)
+{
+	int last;
+
+	last = num % 10;
+
+	return (last);
+}
