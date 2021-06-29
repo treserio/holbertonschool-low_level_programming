3c252eb2fb82bb6d3379441eaf569b515141b22a
Tres Serio <treserio@hotmail.com>
2021-06-29 10:36:23 -0700

    104 init

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - write a list first 100 fibonacci nums
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int cntr = 0;
+	long a, b, c, result;
+
+	a = 1;
+	b = 2;
+	printf("%ld, %ld, ", a, b);
+	result = b;
+
+	while (cntr < 98)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+		if (cntr < 97)
+		{
+			printf("%ld, ", c);
+			++cntr;
+		}
+		else
+		{
+			printf("%ld\n", c);
+			++cntr;
+		}
+	}
+	return (0);
+}
