effa645b12cbcb77e8c1c6b7273a2ebb074917e5
Tres Serio <treserio@hotmail.com>
2021-06-28 22:11:44 -0700

    9.11 broke out for < 40

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,46 +11,30 @@ void times_table(void)
 	{
 		for (x = 0; x < 10; ++x)
 		{
-			if (y == 0)
-				putcomma(y, x, 0);
-			else
-			{
-				result = y * x;
-				if (result > 9)
-				{
-					tens = result / 10;
-					ones = result % 10;
-					_putchar(tens + '0');
-					_putchar(ones + '0');
-					putcomma(y, x, result);
-				}
-				else
-				{
-					_putchar(' ');
-					_putchar(result + '0');
-					putcomma(y, x, result);
-				}
-			}
+			result = y * x;
+			tbl_out(y, x, result);
 		}
 		_putchar('\n');
 	}
 }
 /**
- * putcomma - print ", " where needed
+ * tbl_out - print ", " where needed
  * @out: outer loop value, y
  * @inner: loop value, x
  * @multi: result value, or any int
  * Return: void
  */
-void putcomma(int out, int inner, int multi)
+void tbl_out(int out, int inner, int multi)
 {
+	tens = multi / 10;
+	ones = multi % 10;
+
 	if (out == 0)
 	{
 		_putchar('0');
 		_putchar(',');
 		_putchar(' ');
-	}
-	if (multi < 10)
+	} else if (multi < 10)
 	{
 		_putchar(' ');
 		_putchar(multi + '0');
@@ -59,5 +43,11 @@ void putcomma(int out, int inner, int multi)
 			_putchar(',');
 			_putchar(' ');
 		}
+	} else
+	{
+		_putchar(tens + '0');
+		_putchar(ones + '0');
+		_putchar(',');
+		_putchar(' ');
 	}
 }
