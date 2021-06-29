3723e07370ed6fed8579cdf253c515d265713893
Tres Serio <treserio@hotmail.com>
2021-06-29 07:58:42 -0700

    101.1 corrected betty compiler

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -4,17 +4,17 @@
  *
  * Return: sumation of values % 3 or % 5
  */
-void main(void)
+int main(void)
 {
 	int cntr = 0;
 	long long result = 0;
 
-	while (cntr<1024)
+	while (cntr < 1024)
 	{
 		if (!(cntr % 3) || !(cntr % 5))
 		{
 			result = 0 + cntr;
 		}
-		++cntr
+		++cntr;
 	}
 }
