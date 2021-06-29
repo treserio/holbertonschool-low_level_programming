c5d34994c82f176b81527820d3da670e3b9607ce
Tres Serio <treserio@hotmail.com>
2021-06-28 21:04:36 -0700

    9.0 init

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,21 @@
+#inbluce "holberton.h"
+/**
+ * times_table - print out a times table 0-9
+ *
+ * Return: void
+ */
+
+void times_table(void)
+{
+	int x, y;
+
+	for (y=0; y<10; ++y)
+	{
+		for (x=0; x<10; ++x)
+		{
+			_putchar((y*x));
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+}
