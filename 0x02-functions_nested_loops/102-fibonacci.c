61ee3da336f5996a92c6930409d66db0ba41a2fa
Tres Serio <treserio@hotmail.com>
2021-06-29 07:12:41 -0700

    102.1 bettified

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 /**
  * fibonacci - write a list of fibonacci numbers starting with 1 & 2
  * Return: nothing
@@ -6,13 +7,14 @@
 void fibonacci(void)
 {
 	int cntr, a, b, c;
+
 	a = 1;
 	b = 2;
 
 	for (cntr = 0; cntr < 48; ++cntr)
 	{
-		printf("%d, " a);
-		printf("%d, " b);
+		printf("%d, ", a);
+		printf("%d, ", b);
 		c = a + b;
 		a = b;
 		b = c;
