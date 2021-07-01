87bb1e6160ea05819795ef022a5319bf163eda73
Tres Serio <treserio@hotmail.com>
2021-07-01 13:28:48 -0700

    4.0 init

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * print_numbers - prints [0-9]
+ * Return: nothing
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; ++i)
+	{
+		if (i != 50 || i != 52)
+			_putchar(i);
+	}
+
+	_putchar('\n');
+}
