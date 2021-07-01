13e904c031bf7db757c9133e4d2847bb3bbff711
Tres Serio <treserio@hotmail.com>
2021-07-01 14:26:06 -0700

    7.0 init

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * print_diagonal - prints '\' n times with cumulative spacing for each one
+ * @n: number of times '\' is printed
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	if (n > 0)
+	{
+		int ocnt, icnt;
+
+		for (ocnt = 0; ocnt < n; ++cntr)
+		{
+			for (icnt = 0; icnt < ocnt; icnt++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+		}
+	}
+	_putchar('\n');
+}
