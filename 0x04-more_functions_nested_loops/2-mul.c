d9fe4c9189d5bfdd583515c92d46946d3a4c6481
Tres Serio <treserio@hotmail.com>
2021-07-01 12:42:32 -0700

    2.1 betty check

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,9 +1,10 @@
 /**
  * mul - mulitply 2 values
- * @val1 - first val
- * @val2 - second val
+ * @val1: first val
+ * @val2: second val
+ * Return: val1 * val2
  */
-int mul(int val1, val2)
+int mul(int val1, int val2)
 {
 	return (val1 * val2);
 }
