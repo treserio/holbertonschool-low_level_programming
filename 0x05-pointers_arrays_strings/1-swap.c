fe36be3fc679d1a9d2a5a2ac2d737047c4d27842
treserio <treserio@hotmail.com>
2021-07-02 16:41:14 -0500

    1 init

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * reset_to_98 - swap 2 values through pointers
+ * @thing1: pointer to an int value
+ * @thing2: pointer to an int value
+ * Return: void
+ */
+void swap_int(int *thing1, int *thing2)
+{
+    int holder = *thing1;
+    *thing1 = *thing2;
+    *thing2 = holder;
+}
