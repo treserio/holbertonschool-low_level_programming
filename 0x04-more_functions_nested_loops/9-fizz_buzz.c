e70ebbb46cee1ba42ca77afdb62b0854c46755eb
Tres Serio <treserio@hotmail.com>
2021-07-01 17:11:15 -0700

    9.0 init

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - print 1 to 100 + newline, if % 3 -> Fizz, if % 5 -> Buzz
+ * Description - if both FizzBuzz
+ * Return: void
+ */
+int main(void)
+{
+	int cntr;
+
+	for (cntr = 1; cntr <= 100; ++cntr)
+	{
+		if (!(cntr % 3) && !(cntr % 5))
+			printf("FizzBuzz ");
+		else if (!(cntr % 3))
+			printf("Fizz ");
+		else if (!(cntr % 5))
+			printf("Buzz ");
+		else
+			printf("%d ", cntr);
+	}
+	printf("\n");
+	return (0);
+}
