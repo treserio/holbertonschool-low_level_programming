0c9e88c556318e6cd803756cf2a4f663872efb43
Tres Serio <treserio@hotmail.com>
2021-07-01 22:54:44 -0700

    100 init

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - find the largest prime factor of 612852475143
+ * Return: 0
+ */
+int main (void)
+{
+	long fact = 1, num = 612852475143;
+
+	while (fact < num)
+	{
+		if (num % fact == 0)
+		{
+			num /= fact;
+		}
+		++fact;
+	}
+	printf("%ld\n", fact);
+	return(0);
+}
