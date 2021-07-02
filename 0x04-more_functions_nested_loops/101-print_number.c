b053e688547e1f13ad4b3b7e50fe08fbfb80f4a6
Tres Serio <treserio@hotmail.com>
2021-07-01 20:59:06 -0700

    101.1 tested

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,35 +1,44 @@
 #include "holberton.h"
 /**
- * print_number - print an int of unknown size without long, arrays, or pointers
- *
+ * print_number - print an int of unknown size without long, arrays, pointers
+ * @val: input value to print
  * Return: void
  */
 void print_number(int val)
 {
-	int farLeft = val / 10;
-	int exponent = 1;
+	int far_left;
+	int exponent;
 
 	if (val < 0)
 	{
-		        val *= -1
-					_putchar('-');
+		val *= -1;
+		_putchar('-');
 	}
 
 	while (val > 10)
 	{
+		exponent = 10;
+		far_left = val / 10;
 
 		/* find the last digit */
-		while (farLeft > 10)
+		while (far_left >= 10 && !(far_left % 10 == 0))
 		{
-			farLeft /= 10;
-			exponent++;
+			far_left /= 10;
+			exponent *= 10;
 		}
 
 		/* convert to string and putchar the value */
-		_putchar(farLeft + '0');
+		/* if far_left % 10 = 0 need to print left char & 0 */
+		if (far_left % 10 == 0)
+		{
+			_putchar((far_left / 10) + '0');
+			_putchar('0');
+		}
+		else
+			_putchar(far_left + '0');
 
 		/* remove the farthest left digit */
-		val -= (10**exponent)*farLeft;
+		val -= exponent * far_left;
 	}
-	_putchar(val);
+	_putchar(val+'0');
 }
