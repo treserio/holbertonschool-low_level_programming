6ea0d0ac6b390339c170111e9c92c0f7d83eddd2
treserio <treserio@hotmail.com>
2021-07-02 17:10:34 -0500

    2 init

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _strlen - find the length of a string
+ * @ltr: string pointer
+ * Return: void
+ */
+
+int _strlen(char *ltr)
+{
+	int size;
+
+	while(ltr != "\0")
+	{
+		++size;
+		ltr++;
+	}
+	return (size - 1);
+}
