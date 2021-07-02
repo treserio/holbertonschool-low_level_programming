58fade1bac2a609fec92c4c94b61e8d8dc38826e
Tres Serio <treserio@hotmail.com>
2021-07-02 14:34:40 -0700

    0x05 crted rest and +prototypes

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -2,5 +2,15 @@
 #define HOLBERTON_H
 
 void reset_to_98(int *);
+void swap_int(int *, int *);
+int _strlen(char *);
+void _puts(char *);
+void print_rev(char *);
+void rev_string(char *);
+void puts2(char *);
+void puts_half(char *);
+void print_array(int *, int);
+char *_strcpy(char *, char *);
+int _atoi(char *);
 
 #endif
