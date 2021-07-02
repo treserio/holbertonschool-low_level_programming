c1ab5f17246f5a8eeccb0d979a794d61ee0d8c26
Tres Serio <treserio@hotmail.com>
2021-07-02 14:19:55 -0700

    0x05.1 bettied

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,10 @@
 #include "holberton.h"
 /**
  * reset_to_98 - input a pointer, and update the value it points to
+ * @n: pointer to an int value to be reset
  * Return: void
  */
 void reset_to_98(int *n)
 {
-    *n = 98;
+	*n = 98;
 }
