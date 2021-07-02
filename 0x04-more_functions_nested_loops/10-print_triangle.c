13e404a548178f87d3e1a7470a855ed00ca23079
Tres Serio <treserio@hotmail.com>
2021-07-01 18:13:05 -0700

    10 init

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * print_triangle - print a triangle of "size" size
+ * @size: the height and base of the triangle
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int extra = 1, i;
+
+	if (size > 0)
+	{
+		while (--size)
+		{
+			for (i = 0; i < (size - 1); ++i)
+			{
+				_putchar(' ');
+			}
+			for (i = 0; i < extra; ++i)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+			++extra
+		}
+	}
+	else
+		_putchar('\n');
+}
