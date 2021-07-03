40ba09909854b745602a95c79b77dc37381270c0
treserio <treserio@hotmail.com>
2021-07-02 22:30:35 -0500

    3 init

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * _puts - prints out the string argument one char at a time
+ * @str: pointer to a string array
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int size = 0;
+	char *start = str;
+
+	while (*str != '\0')
+	{
+		++size;
+		str++;
+	}
+
+	while(size--)
+	{
+		_putchar(*start);
+		start++;
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,6 +1,7 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+int _putchar(char);
 void reset_to_98(int *);
 void swap_int(int *, int *);
 int _strlen(char *);
