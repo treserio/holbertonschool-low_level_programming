7f9cbc278abdec83e0c37a76b356ecb36b411531
Tres Serio <treserio@hotmail.com>
2021-07-06 14:49:06 -0500

    101 init

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+int main(void)
+
+{  
+  char holder[89];
+  char *start = &holder, *set = &holder;
+  char current_char;
+  
+  int target = 2772, char_val;
+
+  holder[0] = '2';
+  holder[1] = ' ';
+  set += 2;
+  
+  while(target>0)
+  {
+    char_val = rand() % 95 + 32;
+    
+	if (char_val = 92)
+		continue;
+
+    if (target % char_val > 0)
+    {
+        current_char = char_val;
+        *set = current_char;
+        set++;
+        target -= char_val;
+    }
+    else
+    {
+        *set = target;
+        set++;
+        target -= target;
+    }
+  }
+  *set = '\0';
+  printf("%s", holder);
+  return (0);
+}
