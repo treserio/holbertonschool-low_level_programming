1049df0a43d55e5f9a70a4d9ab6f85e5e477b634
Tres Serio <treserio@hotmail.com>
2021-07-06 13:10:32 -0700

    101.2 rmvd bash special chars

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -4,39 +4,38 @@
 
 int main(void)
 
-{  
-  char holder[89];
-  char *start = holder, *set = holder;
-  char current_char;
-  
-  int target = 2772, char_val;
+{
+	char holder[89], current_char;
+	char *set = holder;
+	int target = 2772, char_val;
 
-  holder[0] = '2';
-  holder[1] = ' ';
-  set += 2;
-  
-  while(target>0)
-  {
-    char_val = rand() % 95 + 32;
-    
-	if (char_val == 92)
-		continue;
+	holder[0] = '2';
+	holder[1] = ' ';
+	set += 2;
 
-    if (target % char_val > 0)
-    {
-        current_char = char_val;
-        *set = current_char;
+	while(target>0)
+	{
+		char_val = rand() % 90 + 32;
+
+		/* remove special bash chars */
+		if (char_val == 92 || char_val == 59 || char_val == 33)
+			continue;
+
+		if (target % char_val > 0)
+		{
+			current_char = char_val;
+			*set = current_char;
         set++;
         target -= char_val;
-    }
-    else
-    {
-        *set = target;
-        set++;
-        target -= target;
-    }
-  }
-  *set = '\0';
-  printf("%s", holder);
-  return (0);
+		}
+		else
+		{
+			*set = target;
+			set++;
+			target -= target;
+		}
+	}
+	*set = '\0';
+	printf("%s", holder);
+	return (0);
 }
