6b147270a04ee8299159eeb4d9b8594d655fe7e9
Tres Serio <treserio@hotmail.com>
2021-07-06 13:35:58 -0700

    101.3 bettied, still wrond

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,7 +3,6 @@
 #include <time.h>
 
 int main(void)
-
 {
 	char holder[89], current_char;
 	char *set = holder;
@@ -13,20 +12,35 @@ int main(void)
 	holder[1] = ' ';
 	set += 2;
 
-	while(target>0)
+	while (target > 0)
 	{
 		char_val = rand() % 90 + 32;
 
 		/* remove special bash chars */
-		if (char_val == 92 || char_val == 59 || char_val == 33)
+		switch (char_val)
+		{
+		case 33:
 			continue;
+		case 39:
+			continue;
+		case 40:
+			continue;
+		case 41:
+			continue;
+		case 42:
+			continue;
+		case 59:
+			continue;
+		case 92:
+			continue;
+		}
 
 		if (target % char_val > 0)
 		{
 			current_char = char_val;
 			*set = current_char;
-        set++;
-        target -= char_val;
+			set++;
+			target -= char_val;
 		}
 		else
 		{
