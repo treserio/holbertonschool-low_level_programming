205ba97703d12d27af9e36132cedf8b1f5f68f57
treserio <treserio@hotmail.com>
2021-07-08 11:01:57 -0500

    probably in the rand line

--- 0x05-pointers_arrays_strings/keygen2.c
+++ 0x05-pointers_arrays_strings/keygen2.c
@@ -10,6 +10,7 @@
 
 int main(void)
 {
+	/* 69 */
 	char ch_list[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789,.-#'?!";
     int target = 0, i;
     char *rtrn_str = NULL;
@@ -17,11 +18,13 @@ int main(void)
     /* seed for rand */
     srand(time(0));
 
+	printf("%d", strlen(ch_list));
+
     while (target != 2772)
     {
         target = 0;
         rtrn_str = malloc(sizeof(char));
-
+/*
         for (i=0; target < 2772; i++)
         {
             int chr = rand() % strlen(ch_list)-1;
@@ -31,6 +34,7 @@ int main(void)
 			printf("%c\n", ch_list[chr]);
 			printf("%s\n", rtrn_str);
         }
+*/
         free(rtrn_str);
     }
 		printf("%s", rtrn_str);
