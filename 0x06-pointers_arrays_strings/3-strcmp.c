3fe69b861bae3957a781b893fbce0f3c1218c9a9
Tres Serio <treserio@hotmail.com>
2021-07-08 15:51:04 -0700

    3.1 bettied

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,20 +1,21 @@
 #include "holberton.h"
 /**
  * _strcmp - compare 2 strings
- * @dest: replace dest with src string till end value
- * @src: string to be copied
- * @end: the value to stop at in the src str
- * Return: pointer to the destination
+ * @s1: first string to compare
+ * @s2: second string to compare
+ * Return: index of deviant char in the strings or zero if match
  */
-char *_strcmp(char *s1, char *s2)
+int _strcmp(char *s1, char *s2)
 {
 	int i, chk = 0;
 
 	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
 		if (s1[i] != s2[i])
+		{
 			chk = i;
 			break;
+		}
 	}
 
 	return (chk);
