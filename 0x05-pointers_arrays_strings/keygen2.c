57f149641fde235383f11c115fa547c83d444470
treserio <treserio@hotmail.com>
2021-07-07 22:34:58 -0500

    keygen2.1

--- 0x05-pointers_arrays_strings/keygen2.c
+++ 0x05-pointers_arrays_strings/keygen2.c
@@ -9,41 +9,55 @@
 
 int main(void)
 {
-    char holder[87], current_char, invalid = "'";
-    char *set = &holder;
-    int target = 2772;
+    char holder[87], current_char, invalid[] = "'!()\"><";
+    char *set = holder;
+    int target = 2772, i;
 
 
-	/* set the seed for rand */
-	srand(time(0));
+    /* set the seed for rand */
+    srand(time(0));
 
     while (target > 0)
     {
         current_char = rand() % 94 + 32;
+        
+        i=0;
 
-		while (invalidstr[i])
-		{
-			if (current_char == invalid[i])
-			{
-				current_char++;
-				i=0;
-			}
-			i++;
-		}
-		if (target - current_char > 170)
+        while (invalid[i])
+        {
+            if (current_char == invalid[i])
+            {
+                current_char++;
+                i=0;
+            }
+            i++;
+        }
+        if (target - current_char > 126)
         {
             *set = current_char;
             set++;
             target -= current_char;
         }
-		else
-		{
-			*set = current_char;
-			set++;
-			*set = target - current_char;
-			target -= target;
-			set++;
-		}
+        else
+        {
+          if (target - current_char > 32)
+          {
+            *set = current_char;
+            set++;
+            target -= current_char;            
+            *set = target;
+            target -= target;
+            set++;
+          }
+          else
+          {
+            *set = 32;
+            target -= 32;
+            *set = target;
+            target -= target;
+            set++;
+          }
+        }
     }
     *set = '\0';
     printf("%s", holder);
