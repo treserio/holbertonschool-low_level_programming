7cc52c79328199007c322f1a43c01fbcab9c95ca
Tres Serio <treserio@hotmail.com>
2021-07-08 09:14:13 -0700

    still strange

--- 0x05-pointers_arrays_strings/keygen2.c
+++ 0x05-pointers_arrays_strings/keygen2.c
@@ -10,31 +10,28 @@
 
 int main(void)
 {
-	/* 69 */
-	char ch_list[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789,.-#'?!";
+	/* 62 + 8 = 70 */
+	char ch_list[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789,.-#'?!/";
     int target = 0, i;
     char *rtrn_str = NULL;
 
     /* seed for rand */
     srand(time(0));
 
-	printf("%d", strlen(ch_list));
-
     while (target != 2772)
     {
         target = 0;
         rtrn_str = malloc(sizeof(char));
-/*
+
         for (i=0; target < 2772; i++)
         {
-            int chr = rand() % strlen(ch_list)-1;
+            int chr = rand() % strlen(ch_list);
             rtrn_str[i] = ch_list[chr];
             target += ch_list[chr];
             rtrn_str = realloc(rtrn_str, (i+2) * sizeof(char));
 			printf("%c\n", ch_list[chr]);
 			printf("%s\n", rtrn_str);
         }
-*/
         free(rtrn_str);
     }
 		printf("%s", rtrn_str);
