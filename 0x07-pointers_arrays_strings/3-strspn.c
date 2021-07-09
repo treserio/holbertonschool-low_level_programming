955dc7ae6c281aa1fcc4565c4a0f4800a09348dc
treserio <treserio@hotmail.com>
2021-07-09 18:36:38 -0500

    3.1 first try

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,8 +1,24 @@
 #include "holberton.h"
+#include "string.h"
 /**
  * _strspn - find the length of a string
  * @ltr: string pointer
  * Return: int value of length of the string
  */
 
-unsigned int _strspn(char *s, char *accept);
+unsigned int _strspn(char *str, char *vals);
+{
+	int cnt = 0, i;
+
+	while(*str)
+	{
+		for (i = 0; vals[i]; ++i)
+		{
+			if (*str == vals[i])
+				++cnt
+				break;
+		}
+		++str;
+	}
+	return (cnt);
+}
