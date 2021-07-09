a2afb1a21bcc529ac2a014dbe1cc1c130d5ff5fa
Tres Serio <treserio@hotmail.com>
2021-07-09 16:52:36 -0700

    3.5 bettied

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,9 +1,10 @@
 #include "holberton.h"
 #include "string.h"
 /**
- * _strspn - find the length of a string
- * @ltr: string pointer
- * Return: int value of length of the string
+ * _strspn - find how long a string is a part of a substring
+ * @str: string pointer to check
+ * @vals: values to comapare string chars against
+ * Return: number of chars from string that are a part of vals
  */
 
 unsigned int _strspn(char *str, char *vals)
@@ -14,7 +15,7 @@ unsigned int _strspn(char *str, char *vals)
 	hldr[0] = *str;
 	hldr[1] = '\0';
 
-	while(strstr(vals, hldr))
+	while (strstr(vals, hldr))
 	{
 		++cnt;
 		++str;
