d74c562904e4b5d0cdc29a3c5a7938cc7d465ed1
treserio <treserio@hotmail.com>
2021-07-09 20:06:59 -0500

    5 init

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * _strstr - returns pointer to first occurance of substring needle in haystack
+ * @haystack: str to search
+ * @needle: substr to locate
+ * Return: Pointer to beginning of substr or null
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+
+	while (*haystack)
+	{
+		for (i=0; haystack[i] == needle[i]; ++i)
+		{
+			if (!needle[i + 1])
+				return (haystack);
+		}
+		++haystack;
+	}
+
+	return (NULL);
+}
