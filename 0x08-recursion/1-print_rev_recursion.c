2cd472f453900f71a2b967fc674d4af25c909129
Tres Serio <treserio@hotmail.com>
2021-07-12 09:35:10 -0700

    1.2 cmplr

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,13 +1,13 @@
 #include "holberton.h"
 /**
- * _puts_rev_recursion - print a string in reverse using a pointer and recurs
+ * _print_rev_recursion - print a string in reverse using a pointer and recurs
  * @spntr: pointer to the string we're wanting printed
  * Return: void
  */
 void _puts_rev_recursion(char *spntr)
 {
 	if (!*spntr)
-		_puts_rev_recursion(++spntr);
+		_print_rev_recursion(++spntr);
 
 	--spntr;
 	_putchar(*spntr);
