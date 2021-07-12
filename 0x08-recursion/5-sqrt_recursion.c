e4b2d0ad34b6e9f025e8d9c1638669a48c130688
treserio <treserio@hotmail.com>
2021-07-12 16:39:29 -0500

    5.3 unresearched attempt 2

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,15 +9,15 @@ int _sqrt_recursion(int num)
 	if (num == 1)
 		return (1);
 	else if (num % 2 == 0)
-		return (2 * _sqrt_recursion(num / 2));
+		return (_sqrt_recursion(num / 2));
 	else if (num % 3 == 0)
-		return (3 * _sqrt_recursion(num / 3));
+		return (_sqrt_recursion(num / 3));
 	else if (num % 5 == 0)
-		return (5 * _sqrt_recursion(num / 5));
+		return (_sqrt_recursion(num / 5));
 	else if (num % 7 == 0)
-		return (7 * _sqrt_recursion(num / 7));
+		return (_sqrt_recursion(num / 7));
 	else if (num % 11 == 0)
-		return (11 * _sqrt_recursion(num / 11));
+		return (_sqrt_recursion(num / 11));
 	else
 		return (-1);
 }
