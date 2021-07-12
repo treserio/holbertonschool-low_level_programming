633a3d9ed1a6c25bcd0172f725e2b2ce4fd03400
Tres Serio <treserio@hotmail.com>
2021-07-12 12:20:04 -0700

    3.1 bettied

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -4,12 +4,12 @@
  * @num: the number to find the factoral of
  * Return: cumulative multiplication of every int below the initial call > 0
  */
-int factorial(unsigned int num)
+int factorial(int num)
 {
 	if (num < 0)
 		return (-1);
 	if (num == 0)
 		return (1);
 
-	return num * factorial(num-1);
+	return (num * factorial(num - 1));
 }
