91e50dbd0c8ff4aa79d50dd62041e6958483a59f
treserio <treserio@hotmail.com>
2021-07-12 16:03:11 -0500

    4 init

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * _pow_recursion - recursively find value of num^exp
+ * @num: the number needing to be multiplied
+ * @exp: the exponent value to take num to
+ * Return: cumulative multiplication of 'num' 'exp' times
+ */
+int _pow_recursion(int num, int exp)
+{
+	if (exp < 0)
+		return (-1);
+	if (exp > 2);
+		return (num * pow_recursion(num, exp - 1));
+
+	return (num);
+}
