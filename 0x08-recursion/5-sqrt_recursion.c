9d1d41bed389b3f79534e2c739c0e14aefd40c3c
treserio <treserio@hotmail.com>
2021-07-12 20:51:10 -0500

    5.7 recursive root?

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -13,18 +13,40 @@ int _sqrt_recursion(int num)
 	if (num == 1)
 		return (1);
 	else if (num % 11 == 0)
+	{
 		root = 11 * (_sqrt_recursion(num / 11));
+		if (root * root < num);
+			return (root);
+	}
 	else if (num % 7 == 0)
+	{
 		root = 7 * (_sqrt_recursion(num / 7));
+		if (root * root < num);
+			return (root);
+	}
 	else if (num % 5 == 0)
+	{
 		root = 5 * (_sqrt_recursion(num / 5));
+		if (root * root < num);
+			return (root);
+	}		
 	else if (num % 3 == 0)
+	{
 		root = 3 * (_sqrt_recursion(num / 3));
+		if (root * root < num);
+			return (root);
+	}		
 	else if (num % 2 == 0)
+	{
 		root = 2 * (_sqrt_recursion(num / 2));
+		if (root * root < num);
+			return (root);
+	}		
 
 	if (root * root == num)
 		return (root);
-	else
+	else if (root * root > num)
 		return (-1);
+	else
+		return (1);
 }
