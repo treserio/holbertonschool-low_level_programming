1a40e2b498b96a507d59f866cd95befee8dd660c
treserio <treserio@hotmail.com>
2021-07-13 12:37:23 -0500

    103.4 added checks

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -4,7 +4,9 @@
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
     int i, carry_ovr = 0, digit, sz1 = 0, sz2 = 0;
+	int *values;
 
+	/* find the ends */
     while (*n1)
 	{
         ++sz1;
@@ -17,14 +19,46 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	}
 	--n1;
 	--n2;
-	printf("%c-%c\n\n", *n1, *n2);
-	/* reverse the val and add to buffer */
-	for (i = 0; i < size_r && i < sz1 && i < sz2; ++i, --n1, --n2)
+
+	/* reverse add the buffer, if the numbers are valid */
+	for (i = 0; i < size_r; ++i, --n1, --n2)
 	{
-		digit = ((*n1 - '0') + (*n2 - '0') + carry_ovr) % 10;
-		r[i] = digit + '0';
-		carry_ovr = ((*n1 - '0') + (*n2 - '0')) / 10;
+		/* if both n1 and n2 in range */
+		if (*n1 >= '0' && *n2 >= '0' && *n1 <= '9' && *n2 <= '9')
+		{
+			values = inf_add_hlpr(*n1, *n2, carry_ovr);
+			r[i] = values[0];
+			carry_ovr = values[1];
+		}
+		/* if *n1 in range and *n2 not */
+		else if (*n1 >= '0' && *n1 <= '9')
+		{
+			values = inf_add_hlpr(*n1, '0', carry_ovr);
+			r[i] = values[0];
+			carry_ovr = values[1];
+		}
+		/* if *n2 in range and *n1 not */
+		else if (*n2 >= '0' && *n2 <= '9')
+		{
+			values = inf_add_hlpr('0', *n2, carry_ovr);
+			r[i] = values[0];
+			carry_ovr = values[1];
+		}
+		/* else continue? */
+		else
+			continue;
 	}
 
 	return (r);
+}
+
+int *inf_add_hlpr(char *n1, char*n2, int carry_ovr)
+{
+	/* array container to return values, 0 = digit, 1 = carry_ovr */
+	int hldr[2];
+
+	hldr[0] = ((*n1 - '0') + (*n2 - '0') + carry_ovr) % 10;
+	hldr[1] = ((*n1 - '0') + (*n2 - '0')) / 10;
+
+	return (hldr);
 }
