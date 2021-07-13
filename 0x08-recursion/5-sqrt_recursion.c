e20d8b737cc41ca268b5b41c50e334ec3de7ee97
treserio <treserio@hotmail.com>
2021-07-12 21:55:07 -0500

    5.8 hlpr

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,5 @@
 #include "holberton.h"
-#include <stdlib.h>
-#include <time.h>
+
 /**
  * _sqrt_recursion - finds the square root of num
  * @num: the number to find the square root of
@@ -8,45 +7,26 @@
  */
 int _sqrt_recursion(int num)
 {
-	int root = 0;
+	return (sqrt_hlpr(num, 1, 1));
+}
 
-	if (num == 1)
-		return (1);
-	else if (num % 11 == 0)
-	{
-		root = 11 * (_sqrt_recursion(num / 11));
-		if (root * root < num);
-			return (root);
-	}
+int sqrt_hlpr(int num, int div, int fact)
+{
+	if (num % 11 == 0)
+		fact *= _sqrt_hlpr(num, num / 11, 11);
 	else if (num % 7 == 0)
-	{
-		root = 7 * (_sqrt_recursion(num / 7));
-		if (root * root < num);
-			return (root);
-	}
+		fact *= _sqrt_hlpr(num, num / 7, 7);
 	else if (num % 5 == 0)
-	{
-		root = 5 * (_sqrt_recursion(num / 5));
-		if (root * root < num);
-			return (root);
-	}		
+		fact *= _sqrt_hlpr(num, num / 5, 5);
 	else if (num % 3 == 0)
-	{
-		root = 3 * (_sqrt_recursion(num / 3));
-		if (root * root < num);
-			return (root);
-	}		
+		fact *= _sqrt_hlpr(num, num / 3, 3);
 	else if (num % 2 == 0)
-	{
-		root = 2 * (_sqrt_recursion(num / 2));
-		if (root * root < num);
-			return (root);
-	}		
-
-	if (root * root == num)
-		return (root);
-	else if (root * root > num)
+		fact *= _sqrt_hlpr(num, num / 2, 2);	
+	
+	if (fact * fact == num)
+		return (fact);
+	else if (fact * fact > num)
 		return (-1);
 	else
-		return (1);
+		return (0);
 }
