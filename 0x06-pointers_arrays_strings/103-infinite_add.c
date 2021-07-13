89cfc4856f9c92cae6764014e9c6e80c138c577e
Tres Serio <treserio@hotmail.com>
2021-07-13 12:02:48 -0700

    103.9 more questions

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -5,7 +5,7 @@ int *inf_add_hlpr(char *n1, char *n2, int carry_ovr);
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
     int i, carry_ovr = 0, sz1 = 0, sz2 = 0;
-	int *values;
+/*	int *values; */
 
 	/* find the ends */
     while (*n1)
@@ -24,27 +24,29 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	/* reverse add the buffer, if the numbers are valid */
 	for (i = 0; i < size_r; ++i, --n1, --n2)
 	{
+		printf("%c+%c=%d|", *n1, *n2, (((*n1 - '0')+(*n2 - '0')+carry_ovr)));
 		/* if both n1 and n2 in range */
 		if (*n1 >= '0' && *n2 >= '0' && *n1 <= '9' && *n2 <= '9')
 		{
-			r[i] = ((*n1 - '0') + (*n2 - '0') + carry_ovr) % 10;
-			carry_ovr = ((*n1 - '0') + (*n2 - '0')) / 10;
+			r[i] = (((*n1 - '0') + (*n2 - '0') + carry_ovr) % 10) + '0';
+			carry_ovr = ((*n1 - '0') + (*n2 - '0') + carry_ovr) / 10;
 		}
 		/* if *n1 in range and *n2 not */
 		else if (*n1 >= '0' && *n1 <= '9')
 		{
-			r[i] = ((*n1 - '0') + (*n2 - '0') + carry_ovr) % 10;
-			carry_ovr = ((*n1 - '0') + (*n2 - '0')) / 10;
+			r[i] = (((*n1 - '0') + (*n2 - '0') + carry_ovr) % 10) + '0';
+			carry_ovr = ((*n1 - '0') + (*n2 - '0') + carry_ovr) / 10;
 		}
 		/* if *n2 in range and *n1 not */
 		else if (*n2 >= '0' && *n2 <= '9')
 		{
-			r[i] = ((*n1 - '0') + (*n2 - '0') + carry_ovr) % 10;
-			carry_ovr = ((*n1 - '0') + (*n2 - '0')) / 10;
+			r[i] = (((*n1 - '0') + (*n2 - '0') + carry_ovr) % 10) + '0';
+			carry_ovr = ((*n1 - '0') + (*n2 - '0') + carry_ovr) / 10;
 		}
 		/* else continue? */
 		else
 			continue;
+
 	}
 
 	return (r);
