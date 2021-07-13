4719917796bfe9699e4df8f50872532fe0cd5023
treserio <treserio@hotmail.com>
2021-07-12 22:27:39 -0500

    6 init

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * is_prime_number - tells if a number is prime or not
+ * @num: the number to test
+ * Return: 1 if num is prime else 0
+ */
+int is_prime_number(int num)
+{
+	if (num % 2)
+		return (0);
+	else
+		return (is_prime_hlpr(num, 3));
+}
+/**
+ * is_prime_hlpr - recursively tells if a num is prime
+ * @num: the number to test
+ * Return: 1 if num is prime else 0
+ */
+is_prime_hlpr(int num, int fact)
+{
+	if (num % fact)
+		return (0);
+	else if (fact > num)
+		return (1);
+	else
+		return (is_prime_hlpr(num, fact + 2));
+}
