80b3c3be0c27264b1b9dac5a757bce222d476e9f
treserio <treserio@hotmail.com>
2021-07-13 15:44:13 -0500

    103.12 refactored end of string check

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -5,6 +5,8 @@ int *inf_add_hlpr(char *n1, char *n2, int carry_ovr);
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
     int i, carry_ovr = 0, sz1 = 0, sz2 = 0;
+	int end1 = 0, end2 = 0;
+	char *rev_n1, *rev_n2;
 
 	/* find the ends */
     while (*n1)
@@ -20,34 +22,67 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	--n1;
 	--n2;
 
-	/* reverse add the buffer, if the numbers are valid */
-	for (i = 0; i < size_r; ++i, --n1, --n2)
+	/* allocate the buffer */
+	rev_n1 = (char *) malloc(sz1);
+	rev_n2 = (char *) malloc(sz2);
+
+	/* reverse strings */
+	for (i=sz1; i > 0; --i)
 	{
-		/* if both n1 and n2 in range */
-		if (*n1 >= '0' && *n2 >= '0' && *n1 <= '9' && *n2 <= '9')
+		*rev_n1 = *n1;
+		++rev_n1;
+		--n1;
+	}
+	for (i=sz2; i > 0; --i)
+	{
+		*rev_n2 = *n2;
+		++rev_n2;
+		--n2;
+	}
+
+	/* add null byte to finish buffer */
+	*rev_n1 = '\0';
+	*rev_n2 = '\0';
+
+
+	/* walk forward through rev strings adding values */
+	for (i = 0; i < size_r; ++i)
+	{
+		/* check if we've reached the end of a string before */
+		if (rev_n1[i] == '\0')
+			end1 = 1;
+		else if (rev_n2[i] == '\0')
+			end2 = 1;
+
+		/* if both n1 and n2 are still strings */
+		/* checks rely on end1 & end2 to know if a string has ended */
+		if (end1 == 0 && end2 == 0)
 		{
-			printf("%c+%c=%d|", *n1, *n2, (((*n1 - '0')+(*n2-'0')+carry_ovr)));
-			r[i] = (((*n1 - '0') + (*n2 - '0') + carry_ovr) % 10) + '0';
-			carry_ovr = ((*n1 - '0') + (*n2 - '0') + carry_ovr) / 10;
+			printf("%c+%c=%d|", rev_n1[i], rev_n2[i], ((rev_n1[i]-'0')+(rev_n2[i]-'0')+carry_ovr));
+			r[i] = (((rev_n1[i]-'0')+(rev_n2[i]-'0')+carry_ovr) % 10) + '0';
+			carry_ovr = ((rev_n1[i]-'0')+(rev_n2[i]-'0')+carry_ovr) / 10;
 		}
-		/* if *n1 in range and *n2 not */
-		else if (*n1 >= '0' && *n1 <= '9')
+		/* if n1 is still a string but n2 is not */
+		else if (end1 == 0)
 		{
-			printf("%c=%d|", *n1, ((*n1 - '0')+carry_ovr) );
-			r[i] = (((*n1 - '0') + carry_ovr) % 10) + '0';
-			carry_ovr = ((*n1 - '0') + carry_ovr) / 10;
+			printf("%c+ =%d|", rev_n1[i], ((rev_n1[i]-'0')+carry_ovr));
+			r[i] = (((rev_n1[i]-'0')+carry_ovr) % 10) + '0';
+			carry_ovr = ((rev_n1[i]-'0')+carry_ovr) / 10;
 		}
-		/* if *n2 in range and *n1 not */
-		else if (*n2 >= '0' && *n2 <= '9')
+		/* if n2 is still a string but n1 is not */
+		else if (end2 == 0)
 		{
-			printf("%c=%d|", *n2, ((*n2 - '0')+carry_ovr) );
-			r[i] = (((*n2 - '0') + carry_ovr) % 10) + '0';
-			carry_ovr = ((*n2 - '0') + carry_ovr) / 10;
+			printf(" +%c=%d|", rev_n2[i], ((rev_n2[i]-'0')+carry_ovr));
+			r[i] = (((rev_n2[i]-'0')+carry_ovr) % 10) + '0';
+			carry_ovr = ((rev_n2[i]-'0')+carry_ovr) / 10;
 		}
-		/* else continue? */
+		/* else fill buffer with null */
 		else
 			r[i] = '\0';
 	}
-
+	
+	/* free up the allocation */
+	free(rev_n1);
+	free(rev_n2);
 	return (r);
 }
