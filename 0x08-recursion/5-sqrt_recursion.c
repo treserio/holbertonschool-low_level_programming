084e6851754b1d1e943f390489cab4641aa4c017
treserio <treserio@hotmail.com>
2021-07-12 22:16:01 -0500

    5.9 simple solv

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-
 /**
  * _sqrt_recursion - finds the square root of num
  * @num: the number to find the square root of
@@ -7,26 +6,20 @@
  */
 int _sqrt_recursion(int num)
 {
-	return (sqrt_hlpr(num, 1, 1));
+	return (sqrt_hlpr(num, 1));
 }
-
-int sqrt_hlpr(int num, int div, int fact)
+/**
+ * _sqrt_recursion - finds the square root of num
+ * @num: the number to find the square root of
+ * @root: the hopeful root of num
+ * Return: cumulative multiplication of 'num' 'exp' times
+ */
+int sqrt_hlpr(int num, int root)
 {
-	if (num % 11 == 0)
-		fact *= _sqrt_hlpr(num, num / 11, 11);
-	else if (num % 7 == 0)
-		fact *= _sqrt_hlpr(num, num / 7, 7);
-	else if (num % 5 == 0)
-		fact *= _sqrt_hlpr(num, num / 5, 5);
-	else if (num % 3 == 0)
-		fact *= _sqrt_hlpr(num, num / 3, 3);
-	else if (num % 2 == 0)
-		fact *= _sqrt_hlpr(num, num / 2, 2);	
-	
-	if (fact * fact == num)
-		return (fact);
-	else if (fact * fact > num)
+	if (root * root == num)
+		return (root);
+	else if (root * root > num)
 		return (-1);
 	else
-		return (0);
+		return (sqrt_hlpr(num, root+1));
 }
