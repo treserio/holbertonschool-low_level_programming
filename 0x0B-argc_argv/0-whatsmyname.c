111994f0589d0dd2ca8b2fd2e8ab8affc3ac4e71
treserio <treserio@hotmail.com>
2021-07-15 14:28:11 -0500

    0 init, and edited ignore

---
+++ 0x0B-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - find the length of a string
+ * @argc: an int value defining the number of arguements passed into argv
+ * @argv: an array of null-term str representing cmnd-line args
+ * Return: zero
+ */
+int main (int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
