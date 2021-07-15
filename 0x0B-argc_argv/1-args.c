ca2dab91ac5702ff3879f23cccecdb2783960eed
treserio <treserio@hotmail.com>
2021-07-15 15:24:05 -0500

    2 init

--- 0x0B-argc_argv/1-args.c
+++ 0x0B-argc_argv/1-args.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- * main - find the length of a string
+ * main - print the number of arguements fed to the porgram
  * @argc: an int value defining the number of arguements passed into argv
  * @argv: an array of null-term str representing cmnd-line args
  * Return: zero
@@ -9,6 +9,6 @@
 int main (int argc, char *argv[])
 {
 	argv[1] = "thing";
-	printf("%d\n", argc);
+	printf("%d\n", argc - 1);
 	return (0);
 }
---
+++ 0x0B-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - print the arguements fed to the program
+ * @argc: an int value defining the number of arguements passed into argv
+ * @argv: an array of null-term str representing cmnd-line args
+ * Return: zero
+ */
+int main (int argc, char *argv[])
+{
+	for (i = 0; i < argc; ++i)
+		printf("%s", argv[i]);
+
+	return (0);
+}
