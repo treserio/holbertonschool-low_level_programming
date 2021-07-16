d9888b104cf641a1c0c78c1cce1b1006c299f43e
treserio <treserio@hotmail.com>
2021-07-16 12:48:58 -0500

    4.1 corrected cmnts

--- 0x0B-argc_argv/4-add.c
+++ 0x0B-argc_argv/4-add.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - multiply 2 values input through arguements
+ * main - cumulatively add all args
  * @argc: an int value defining the number of arguements passed into argv
  * @argv: an array of null-term str representing cmnd-line args
  * Return: zero
