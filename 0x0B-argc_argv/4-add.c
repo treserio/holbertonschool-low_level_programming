8ecd91345046cbb1a25765547f28fbaa03f7d264
treserio <treserio@hotmail.com>
2021-07-16 12:37:19 -0500

    3 & 4 init

---
+++ 0x0B-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiply 2 values input through arguements
+ * @argc: an int value defining the number of arguements passed into argv
+ * @argv: an array of null-term str representing cmnd-line args
+ * Return: zero
+ */
+int main(int argc, char *argv[])
+{
+	int sum;
+
+	++argv;
+	while(*argv)
+	{
+		if (atoi(*argv))
+			sum += atoi(*argv);
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+		++argv;
+	}
+
+	printf("%d", sum);
+	return (0);
+}
