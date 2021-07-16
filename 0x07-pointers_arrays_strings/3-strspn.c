926062113735f04cab5d3631c435f1c2542a2641
treserio <treserio@hotmail.com>
2021-07-16 18:11:17 -0500

    3.01 3-strspn rework

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,21 +10,19 @@
 unsigned int _strspn(char *str, char *vals)
 {
 	unsigned int cnt = 0;
-	int i = 0;
+	char hldr[2];
 
-	while (*str)
+	hldr[0] = *str;
+	hldr[1] = '\0';
+
+	while (strstr(vals, hldr))
 	{
-		for (i = 0; vals[i] != 0; i++)
-		{
-			if (*str == vals[i])
-			{
-				cnt++;
-				break;
-			}
-			else if (!vals[i + 1])
-				return (cnt);
-		}
-		str++;
+		++cnt;
+		++str;
+		if (*str)
+			hldr[0] = *str;
+		else
+			break;
 	}
 	return (cnt);
 }
