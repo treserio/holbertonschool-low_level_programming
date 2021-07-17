5ff571b68f01bea016190e6d65e9587c6a765871
treserio <treserio@hotmail.com>
2021-07-17 17:07:43 -0500

    0x06 final rework?

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,116 +1,57 @@
-#include "holberton.h"
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
-int *inf_add_hlpr(char *n1, char *n2, int carry_ovr);
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
+char *infinite_add(char *n1, char *n2, char *arr, int size_r)
 {
-	int i, carry_ovr = 0, sz1 = 0, sz2 = 0;
-	int end1 = 0, end2 = 0;
-	char *rev_n1, *rev_n2, *rev_r;
+	int sz1 = 0, sz2 = 0, i = 0, carry_over = 0;
 
-	/* find the ends */
-	while (*n1)
+	/* move pointer to end of strings */
+	while (n1[sz1])
+        ++sz1;		
+	while (n2[sz2])
+        ++sz2;
+
+	/* reverse iterate back over them and add to buffer */
+	for (i = 0, --sz1, --sz2; (sz1 >= 0 || sz2 >= 0) ; ++i, --sz1, --sz2)
 	{
-		++sz1;
-		++n1;
-	}
-	while (*n2)
-	{
-		++sz2;
-		++n2;
-	}
-	--n1;
-	--n2;
-
-	/* allocate the buffer */
-	rev_n1 = (char *) malloc(sizeof(char) * (sz1 + 1));
-	rev_n2 = (char *) malloc(sizeof(char) * (sz2 + 1));
-	rev_r = (char *) malloc(sizeof(char) * size_r);
-
-	/* reverse strings */
-	for (i = sz1; i > 0; --i)
-	{
-		*rev_n1 = *n1;
-		++rev_n1;
-		--n1;
-	}
-	for (i = sz2; i > 0; --i)
-	{
-		*rev_n2 = *n2;
-		++rev_n2;
-		--n2;
-	}
-
-	/* add null byte to finish buffer */
-	*rev_n1 = '\0';
-	*rev_n2 = '\0';
-
-	rev_n1 = rev_n1 - (sz1);
-	rev_n2 = rev_n2 - (sz2);
-
-	/* walk forward through rev strings adding values */
-	for (i = 0; i < size_r; ++i, ++rev_n1, ++rev_n2)
-	{
-		/* check if we've reached the end of a string before */
-		if (*rev_n1 == '\0' && *rev_n2 == '\0')
-		{
-			end1 = 1;
-			end2 = 1;
-		}
-		else if (*rev_n1 == '\0')
-			end1 = 1;
-		else if (*rev_n2 == '\0')
-			end2 = 1;
-
 		/* if both n1 and n2 are still strings */
-		/* checks rely on end1 & end2 to know if a string has ended */
-		if (end1 == 0 && end2 == 0)
+		printf("%d, %d\n", sz1, sz2);
+		if (sz1 >= 0 && sz2 >= 0)
 		{
-			r[i] = (((*rev_n1 - '0') + (*rev_n2 - '0') + carry_ovr) % 10) + '0';
-			carry_ovr = ((*rev_n1 - '0') + (*rev_n2 - '0') + carry_ovr) / 10;
+			arr[i] = (((n1[sz1] - '0') + (n2[sz2] - '0') + carry_over) % 10) + '0';
+			carry_over = ((n1[sz1] - '0') + (n2[sz2] - '0') + carry_over) / 10;
 		}
 		/* if n1 is still a string but n2 is not */
-		else if (end1 == 0)
+		else if (sz1 >= 0)
 		{
-			r[i] = (((*rev_n1 - '0') + carry_ovr) % 10) + '0';
-			carry_ovr = ((*rev_n1 - '0') + carry_ovr) / 10;
+			arr[i] = (((n1[sz1] - '0') + carry_over) % 10) + '0';
+			carry_over = ((n1[sz1] - '0') + carry_over) / 10;
 		}
 		/* if n2 is still a string but n1 is not */
-		else if (end2 == 0)
+		else if (sz2 >= 0)
 		{
-			r[i] = (((*rev_n2 - '0') + carry_ovr) % 10) + '0';
-			carry_ovr = ((*rev_n2 - '0') + carry_ovr) / 10;
+			arr[i] = (((n2[sz2] - '0') + carry_over) % 10) + '0';
+			carry_over = ((n2[sz2] - '0') + carry_over) / 10;
 		}
-		/* check for carry_ovr */
-		else if (carry_ovr == 1)
+		/* check for carry_ovr, probably unecessary with the current for loop */
+		else if (carry_over == 1)
 		{
-			r[i] = carry_ovr + '0';
-			carry_ovr = 0;
+			arr[i] = carry_over + '0';
+			carry_over = 0;
 		}
-		/* else fill buffer with null */
 		else
-			r[i] = '\0';
-
+			arr[i] = '\0';
 	}
-	/* find size of result */
-	sz1 = 0;
-	while (r[sz1])
-		++sz1;
+	/* check for carry over */
+	if (carry_over == 1)
+		arr[i++] = carry_over + '0';
 
-	--sz1;
-	/* reverse the result */
-	for (i = 0; i <= sz1; ++i)
-	{
-		rev_r[i] = r[sz1 - i];
-	}
-
-	++i;
-	if (i > size_r)
+	/* check if value is > size_r, still needs null term char */
+	if (i > size_r - 1)
 		return (0);
 
-	for (; i < size_r; ++i)
-		r[i] = '\0';
+	/* end buffer with null char */
+	for (; i <= size_r; i++)
+	{	
+		arr[i] = '\0';
+	}
 
-	return (rev_r);
+	return (arr);
 }
