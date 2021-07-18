a020beae12ee78042e9fbfe9b67c57a141ae6bbe
treserio <treserio@hotmail.com>
2021-07-18 12:00:17 -0500

    0x06.103 forgot to rev dest string

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -7,17 +7,14 @@
  * @size_r: the size of the output buffer
  * Return: the sum of n1 + n2
  */
-
 char *infinite_add(char *n1, char *n2, char *arr, int size_r)
 {
 	int sz1 = 0, sz2 = 0, i = 0, carry_over = 0;
-
-	/* move pointer to end of strings */
+	/* find input sizes */
 	while (n1[sz1])
-		++sz1;
+        ++sz1;		
 	while (n2[sz2])
-		++sz2;
-
+        ++sz2;
 	/* reverse iterate back over them and add to buffer */
 	for (i = 0, --sz1, --sz2; (sz1 >= 0 || sz2 >= 0) ; ++i, --sz1, --sz2)
 	{
@@ -39,28 +36,23 @@ char *infinite_add(char *n1, char *n2, char *arr, int size_r)
 			arr[i] = (((n2[sz2] - '0') + carry_over) % 10) + '0';
 			carry_over = ((n2[sz2] - '0') + carry_over) / 10;
 		}
-		/* check for carry_ovr, probably unecessary with the current for loop */
-		else if (carry_over == 1)
-		{
-			arr[i] = carry_over + '0';
-			carry_over = 0;
-		}
-		else
-			arr[i] = '\0';
 	}
 	/* check for carry over */
 	if (carry_over == 1)
 		arr[i++] = carry_over + '0';
-
 	/* check if value is > size_r, still needs null term char */
 	if (i > size_r - 1)
 		return (0);
-
-	/* end buffer with null char */
-	for (; i <= size_r; i++)
+	/* reverse the string */
+	/* reuse carry_over for 1/2 way through i, and sz1 for temp val */
+	carry_over = --i;
+	for (; i > carry_over / 2; --i)
 	{
-		arr[i] = '\0';
+		sz1 = arr[i];
+		arr[i] = arr[carry_over - i];
+		arr[carry_over - i] = sz1;
 	}
-
+	/* end buffer with null char */
+	arr[carry_over+1] = '\0';
 	return (arr);
 }
