75c56199fd6601731bac9305b48b3cca827ab022
Tres Serio <treserio@hotmail.com>
2021-07-18 10:06:30 -0700

    0x06.103 rmvd comments for betty, h8 this

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -10,41 +10,35 @@
 char *infinite_add(char *n1, char *n2, char *arr, int size_r)
 {
 	int sz1 = 0, sz2 = 0, i = 0, carry_over = 0;
-	/* find input sizes */
+
 	while (n1[sz1])
-        ++sz1;		
+		++sz1;
 	while (n2[sz2])
-        ++sz2;
+		++sz2;
 	/* reverse iterate back over them and add to buffer */
 	for (i = 0, --sz1, --sz2; (sz1 >= 0 || sz2 >= 0) ; ++i, --sz1, --sz2)
 	{
-		/* if both n1 and n2 are still strings */
 		if (sz1 >= 0 && sz2 >= 0)
 		{
 			arr[i] = (((n1[sz1] - '0') + (n2[sz2] - '0') + carry_over) % 10) + '0';
 			carry_over = ((n1[sz1] - '0') + (n2[sz2] - '0') + carry_over) / 10;
 		}
-		/* if n1 is still a string but n2 is not */
 		else if (sz1 >= 0)
 		{
 			arr[i] = (((n1[sz1] - '0') + carry_over) % 10) + '0';
 			carry_over = ((n1[sz1] - '0') + carry_over) / 10;
 		}
-		/* if n2 is still a string but n1 is not */
 		else if (sz2 >= 0)
 		{
 			arr[i] = (((n2[sz2] - '0') + carry_over) % 10) + '0';
 			carry_over = ((n2[sz2] - '0') + carry_over) / 10;
 		}
 	}
-	/* check for carry over */
 	if (carry_over == 1)
 		arr[i++] = carry_over + '0';
-	/* check if value is > size_r, still needs null term char */
 	if (i > size_r - 1)
 		return (0);
 	/* reverse the string */
-	/* reuse carry_over for 1/2 way through i, and sz1 for temp val */
 	carry_over = --i;
 	for (; i > carry_over / 2; --i)
 	{
@@ -53,6 +47,6 @@ char *infinite_add(char *n1, char *n2, char *arr, int size_r)
 		arr[carry_over - i] = sz1;
 	}
 	/* end buffer with null char */
-	arr[carry_over+1] = '\0';
+	arr[carry_over + 1] = '\0';
 	return (arr);
 }
