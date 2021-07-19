811e1a0606b2c0cee5467dfc643df9896f67c205
treserio <treserio@hotmail.com>
2021-07-18 22:32:58 -0500

    104 init

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,29 @@
+
+void print_buffer(char *str, int size)
+{
+	int i, j;
+
+	if (size == 0)
+		printf("\n");
+
+	/* print size chars from str */
+	for (i = 0; str[i] && i < size; ++i)
+	{
+		/* position of first byte of every 10 */
+		if (i % 10 == 0)
+			printf("%08x: ", i);
+		/* hex content 2 chars at a time, 1 % 2 = 1, every odd */
+		if (i % 2)
+			printf("%02x%02x", str[i -1], str[i]);
+		/* if at the end of the line, print out the last 10 chars of the string */
+		if (i % 10 == 9)
+		{
+			printf(" ");
+			for (j = i - 9; j <= i; ++j)
+				printf(str[j]);
+			printf("\n");
+		}
+		else
+			printf(" ");
+	}
+}
