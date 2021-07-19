467cc924d0867cdbecb460b61914b440959c4304
Tres Serio <treserio@hotmail.com>
2021-07-18 20:50:59 -0700

    104.1 needs printable char check

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,4 +1,5 @@
-
+#include <stdio.h>
+#include "holberton.h"
 void print_buffer(char *str, int size)
 {
 	int i, j;
@@ -7,23 +8,24 @@ void print_buffer(char *str, int size)
 		printf("\n");
 
 	/* print size chars from str */
-	for (i = 0; str[i] && i < size; ++i)
+	for (i = 0; i < size; ++i)
 	{
 		/* position of first byte of every 10 */
 		if (i % 10 == 0)
-			printf("%08x: ", i);
+			printf("%08x:", i);
 		/* hex content 2 chars at a time, 1 % 2 = 1, every odd */
 		if (i % 2)
-			printf("%02x%02x", str[i -1], str[i]);
+			printf("%02x%02x", str[i - 1], str[i]);
+		else
+			printf(" ");
 		/* if at the end of the line, print out the last 10 chars of the string */
 		if (i % 10 == 9)
 		{
 			printf(" ");
 			for (j = i - 9; j <= i; ++j)
-				printf(str[j]);
+				/* add check for printable char */
+				printf("%c", str[j]);
 			printf("\n");
 		}
-		else
-			printf(" ");
 	}
 }
