7ce85671c7fb092cba2c603fd12a2fbf5c983acf
treserio <treserio@hotmail.com>
2021-07-20 13:30:08 -0500

    2.2 removed else if from NULL chk, bettied

--- 0x0C-malloc_free/2-str_concat.c
+++ 0x0C-malloc_free/2-str_concat.c
@@ -5,7 +5,7 @@
  * str_concat - contatonate 2 strings
  * @s1: the first string to add s2 to
  * @s2: string to add to the end of s1
- * Return: pntr to result, or NULL if s1 & s2 are NULL or out of 
+ * Return: pntr to result, or NULL if s1 & s2 are NULL or out of
  */
 char *str_concat(char *s1, char *s2)
 {
@@ -15,7 +15,7 @@ char *str_concat(char *s1, char *s2)
 	/* reassign NULL values to "" */
 	if (s1 == NULL)
 		s1 = "";
-	else if (s2 == NULL)
+	if (s2 == NULL)
 		s2 = "";
 
 	res = malloc(sizeof(char) * (strlen(s1) + strlen(s2) + 1));
@@ -28,7 +28,7 @@ char *str_concat(char *s1, char *s2)
 	{
 		res[size] = *s2;
 	}
-	
+
 	res[size + 1] = '\0';
 	return (res);
 }
