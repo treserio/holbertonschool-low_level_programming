f9be8cb4b76e17b01c39d062d56dc1e7c7fa7a2d
Tres Serio <treserio@hotmail.com>
2021-07-23 10:40:27 -0700

    1.1 bettied

--- 0x0E-more_malloc_free/1-string_nconcat.c
+++ 0x0E-more_malloc_free/1-string_nconcat.c
@@ -1,26 +1,29 @@
 #include "holberton.h"
 #include <stdlib.h>
+#include <stdio.h>
 /**
- * malloc_checked - malloc memory and confirm success
- * @num: amount of memory to allocate
- * Return: pointer to memory, or exit code 98
+ * string_nconcat - malloc memory and concatonate S2 to S1 num chars
+ * @s1: initial string
+ * @s2: second string to add to s1 num places
+ * @num: number of chars to add from s2
+ * Return: pointer to concatonated string, or NULL if fail
  */
 char *string_nconcat(char *s1, char *s2, unsigned int num)
 {
-	int i, j;
+	unsigned int i, j;
 	char *concat;
-	
+
 	/* convert NULLs to "" */
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	
-	concat = malloc(iof(s1) + num);
+
+	concat = malloc(sizeof(s1) + num);
 	if (concat == NULL)
 		return (NULL);
 
-	for(i = 0; s1[i]; ++i)
+	for (i = 0; s1[i]; ++i)
 		concat[i] = s1[i];
 
 	for (j = 0; s2[j] && j < num; ++j, ++i)
