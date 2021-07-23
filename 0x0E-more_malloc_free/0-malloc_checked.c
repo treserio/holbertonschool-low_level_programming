8062bfd726dcc065980c69afee98978e145929bb
treserio <treserio@hotmail.com>
2021-07-23 11:18:19 -0500

    0 init

---
+++ 0x0E-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * strtow - split strings into words
+ * @str: string to split
+ * Return: pointer to an array of words
+ */
+void *malloc_checked(unsigned int num)
+{
+	void *thing = malloc(num);
+	if (thing == NULL)
+		exit (98);
+	else
+		return (thing);
+}
