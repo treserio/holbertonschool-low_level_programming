ff5687989643b48c632e11aacdb82d7c47c0286e
treserio <treserio@hotmail.com>
2021-07-23 22:47:35 -0500

    100 init

---
+++ 0x0E-more_malloc_free/100-realloc.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * array_range - create an array of ints from min to max
+ * @ptr: the pointer to the previously allocated memory
+ * @osz: original size of buffer
+ * @nsz: new size of buffer
+ * Return: pntr to newly assigned memory, with values copied
+ */
+void *_realloc(void *ptr, unsigned int osz, unsigned int nsz)
+{
+	int i;
+	char *dest, *orig = ptr;	
+
+	/* new_size == old_size rtrn ptr */
+	if (osz == nsz)
+		return (ptr);
+
+	if (nsz == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (ptr == NULL)
+		return (malloc(osz + nsz));
+
+	/* copy values from input pointer to dest */
+	for (i = 0; i < nsz; ++i)
+		dest[i] = orig[i];
+
+	free(ptr);
+	return (dest);
+}
