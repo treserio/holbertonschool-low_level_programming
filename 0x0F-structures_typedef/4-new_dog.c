c7bb00c77d4805d15d70801125045824aede383f
Tres Serio <treserio@hotmail.com>
2021-07-26 14:54:52 -0500

    4 init

--- 0x0F-structures_typedef/4-new_dog.c
+++ 0x0F-structures_typedef/4-new_dog.c
@@ -1,6 +1,21 @@
 #include "dog.h"
+#include <stdlib.h>
 /**
  * init_dog - initialize a dog struct
  * @d: pointer the dog struct destination
  * Return: void
  */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	struct dog *fido;
+	
+	if (fido == NULL)
+	{
+		free(fido);
+		return (NULL);
+	}
+	else
+		init_dog(fido, name, age, owner);
+
+	return (fido);
+}
