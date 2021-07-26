9ac95952c0e5ce77846211dc5d0e4c12fc260f18
Tres Serio <treserio@hotmail.com>
2021-07-26 15:53:44 -0500

    4.5 refactor

--- 0x0F-structures_typedef/4-new_dog.c
+++ 0x0F-structures_typedef/4-new_dog.c
@@ -1,80 +1,4 @@
 #include "dog.h"
-#include <stdlib.h>
-/**
- * _strdup - create a duplicate array of str
- * @str: the string to copy
- * Return: pntr to dup, or NULL if str NULL or insufficient mem
- */
-char *_strdup(char *str)
-{
-	unsigned int size = 0;
-	char *arr;
-
-	if (str == NULL)
-		return (NULL);
-
-	/* get size of str */
-	while (str[size])
-		++size;
-
-	/* allocate memory for cpy buffer, size + 1 for NULL byte */
-	arr = malloc(sizeof(char) * size + 1);
-
-	if (arr != NULL)
-	{
-		/* repurpose size */
-		size = 0;
-
-		/* copy values over */
-		while (str[size])
-		{
-			arr[size] = str[size];
-			++size;
-		}
-		arr[size] = '\0';
-		return (arr);
-	}
-	else
-	{
-		free(arr);
-		return (NULL);
-	}
-}
-/**
- * init_dog - initialize a dog struct
- * @d: pointer the dog struct destination
- * @name: dog's name
- * @age: dog's age
- * @owner: dog's owner
- * Return: void
- */
-void init_dog(struct dog *d, char *name, float age, char *owner)
-{
-	if (d)
-	{
-		(*d).name = _strdup(name);
-		if ((*d).name == NULL)
-		{
-			free(d);
-			return (NULL);
-		}
-		(*d).age = age;
-		if ((*d).age < 0)
-		{
-			free((*d).name);
-			free(d);
-			return (NULL);
-		}
-		(*d).owner = _strdup(owner);
-		if ((*d).owner == NULL)
-		{
-			free((*d).name);
-			free((*d).age);
-			free(d);
-			return (NULL);
-		}
-	}
-}
 /**
  * new_dog - create a new dog struct and initialize it's values
  * @name: string for dog's name
@@ -84,15 +8,15 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	struct dog *fido = malloc(sizeof(struct dog));
+	struct dog fido;
+	
+	fido = { name, age, owner }
 
 	if (fido == NULL)
 	{
 		free(fido);
 		return (NULL);
 	}
-	else
-		init_dog(fido, name, age, owner);
 
 	return (fido);
 }
