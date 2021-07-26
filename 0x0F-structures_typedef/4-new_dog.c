8c00d0acfd4289423968ae401ae22616fd72eb2a
Tres Serio <treserio@hotmail.com>
2021-07-26 15:26:00 -0500

    4.1 added init function

--- 0x0F-structures_typedef/4-new_dog.c
+++ 0x0F-structures_typedef/4-new_dog.c
@@ -3,6 +3,23 @@
 /**
  * init_dog - initialize a dog struct
  * @d: pointer the dog struct destination
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		(*d).name = name;
+		(*d).age = age;
+		(*d).owner = owner;
+	}
+}
+/**
+ * init_dog - initialize a dog struct
+ * @d: pointer the dog struct destination
  * Return: void
  */
 dog_t *new_dog(char *name, float age, char *owner)
