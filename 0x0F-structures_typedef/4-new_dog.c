e52047514f960ed965202c8b048d8a794631663a
Tres Serio <treserio@hotmail.com>
2021-07-26 15:37:15 -0500

    4.3 bettied

--- 0x0F-structures_typedef/4-new_dog.c
+++ 0x0F-structures_typedef/4-new_dog.c
@@ -18,9 +18,11 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	}
 }
 /**
- * init_dog - initialize a dog struct
- * @d: pointer the dog struct destination
- * Return: void
+ * new_dog - create a new dog struct and initialize it's values
+ * @name: string for dog's name
+ * @age: float value for age
+ * @owner: string for owner's name
+ * Return: pointer to dog_t struct
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
