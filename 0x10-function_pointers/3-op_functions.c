1f596550ede198450c8eb77578c37a9d1e7010a5
treserio <treserio@hotmail.com>
2021-07-27 18:29:27 -0500

    3-op_func added 0 check to / & %

--- 0x10-function_pointers/3-op_functions.c
+++ 0x10-function_pointers/3-op_functions.c
@@ -36,7 +36,11 @@ int op_mul(int a, int b)
  */
 int op_div(int a, int b)
 {
-	return (a / b);
+	if (b != 0)
+		return (a / b);
+	else
+		printf("Error\n");
+		exit(100);
 }
 /**
  * op_mod - divide two numbers and return remainder
@@ -46,5 +50,9 @@ int op_div(int a, int b)
  */
 int op_mod(int a, int b)
 {
-	return (a % b);
+	if (b != 0)
+		return (a % b);
+	else
+		printf("Error\n");
+		exit(100);
 }
