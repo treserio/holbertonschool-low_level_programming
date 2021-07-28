9ba3d4750cff43af20e05da274c697f78da17cac
Tres Serio <treserio@hotmail.com>
2021-07-27 18:09:18 -0700

    3-get while chk NULL first

--- 0x10-function_pointers/3-get_op_func.c
+++ 0x10-function_pointers/3-get_op_func.c
@@ -19,7 +19,7 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (strcmp(ops[i].op, s) != 0 && ops[i].op != NULL)
+	while (ops[i].op != NULL && strcmp(ops[i].op, s))
 		++i;
 
 	return (ops[i].f);
