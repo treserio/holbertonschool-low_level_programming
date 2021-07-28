7acea40913a861ac4be2a2b7571788a79dac22e2
Tres Serio <treserio@hotmail.com>
2021-07-27 18:36:11 -0700

    3.1 req chk 12

--- 0x10-function_pointers/3-get_op_func.c
+++ 0x10-function_pointers/3-get_op_func.c
@@ -19,7 +19,7 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (ops[i].op != NULL && strcmp(ops[i].op, s))
+	while (ops[i].op != NULL && strcmp(ops[i].op, s) != 0)
 		++i;
 
 	return (ops[i].f);
