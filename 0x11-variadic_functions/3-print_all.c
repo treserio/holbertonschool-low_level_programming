fde34812bd66093c60346be659c223e5b6e76c3a
Tres Serio <treserio@hotmail.com>
2021-07-29 23:09:25 -0700

    3.14 stupid reqs

--- 0x11-variadic_functions/3-print_all.c
+++ 0x11-variadic_functions/3-print_all.c
@@ -7,12 +7,11 @@
  */
 void print_all(const char * const format, ...)
 {
-	int i = 0, j;
+	int i = 0, seg = 0;
 	va_list args;
 	char *str;
 
 	va_start(args, format);
-
 	while (format && format[i])
 	{
 		/* switch case to test what va_arg type to pass function */
@@ -20,24 +19,31 @@ void print_all(const char * const format, ...)
 		{
 			case 'c':
 				printf("%c", va_arg(args, int));
+				seg = 1;
 				break;
 			case 'i':
 				printf("%d", va_arg(args, int));
+				seg = 1;
 				break;
 			case 'f':
 				printf("%f", va_arg(args, double));
+				seg = 1;
 				break;
 			case 's':
 				str = va_arg(args, char *);
 				if (str == NULL)
 					str = "(nil)";
 				printf("%s", str);
+				seg = 1;
 				break;
 		}
 		++i;
-		printf(", ");
+		if (seg == 1 && format[i] != '\0')
+		{
+			printf(", ");
+			seg = 0;
+		}
 	}
 	va_end(args);
-
 	printf("\n");
 }
