cbb1e67f113ac2f3dffd7b62a361f5c7f8a828ea
treserio <treserio@hotmail.com>
2021-08-06 14:37:52 -0500

    0 init

---
+++ 0x13-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * print_list - print the str of linked list and return values in list
+ * @node: starting node to print in list
+ * Return: number of items in list
+ */
+size_t print_list(const list_t *node)
+{
+	int i = 0;
+
+	if (node)
+	{
+		while (node)
+		{
+			if (node->str == NULL)
+				printf("[0] (nil)\n");
+			else
+				printf("[%d] %s\n", node->len, node->str);
+			++i;
+			node = (*node).next;
+		}
+	}
+
+	return (i);
+}
