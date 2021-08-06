41b8ef3878626970402ed6077bfd9539e62d9462
Tres Serio <treserio@hotmail.com>
2021-08-06 16:32:13 -0700

    100.1 bettied

--- 0x13-singly_linked_lists/100-first.c
+++ 0x13-singly_linked_lists/100-first.c
@@ -7,5 +7,8 @@
 
 void __attribute__((constructor)) early_bird()
 {
-printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	char *str1 = "You're beat! and yet, you must allow,";
+	char *str2 = "\nI bore my house upon my back!\n";
+
+	printf("%s%s", str1, str2);
 }
