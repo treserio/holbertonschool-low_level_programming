f0d9f4b28cfece8651f55a72f162c4e4f7c100ae
Tres Serio <treserio@hotmail.com>
2021-08-11 14:53:11 -0700

    6.1 cmplr debugging

--- 0x14-more_singly_linked_lists/6-pop_listint.c
+++ 0x14-more_singly_linked_lists/6-pop_listint.c
@@ -2,19 +2,20 @@
 /**
  * delete_nodeint_at_index - free all elements of a list
  * @head: starting node in list
+ * @index: value to remove
  * Return: void
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	unsigned int i;
-	linstint_t *tmp;
+	listint_t *tmp;
 
-	for (i = 0; head && *head && i <= index; ++i, (*head)->next)
+	for (i = 0; head && *head && i <= index; ++i, *head = (*head)->next)
 	{
 		if (i == 0)
 		{
 			tmp = *head;
-			head = (*head)->next;
+			head = &(*head)->next;
 			free(tmp);
 		}
 		else if (i == index - 1)
@@ -22,4 +23,6 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 	tmp->next = (*head)->next;
 	free(head);
+
+	return ((*head)->n);
 }
