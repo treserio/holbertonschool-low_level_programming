55b1bc71ae3b98882841a7c081b8f3afa1da58b7
treserio <treserio@hotmail.com>
2021-08-11 11:35:55 -0500

    2.1 fixed comments

---
+++ 0x14-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * add_nodeint - add a node to the front of the list
+ * @head: address of first node pointer
+ * @n: int value for new node
+ * Return: NULL if fail, or ptr to first item of list
+ */
+list_t *add_nodeint(listint_t **head, const int n)
+{
+	unsigned int sz = 0;
+	listint_t *new = malloc(sizeof(listint_t));
+
+	if (!new || !head || !n)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	new->n = n;
+	new->next = *head;
+
+	*head = new;
+
+	return (*head);
+}
