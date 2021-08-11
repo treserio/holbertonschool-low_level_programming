28495d9e5c0fd5d460167969e50fd68b7980d376
Tres Serio <treserio@hotmail.com>
2021-08-11 09:48:47 -0500

    header setup

---
+++ 0x14-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * print_listint - print the str of linked list and return values in list
+ * @node: starting node to print in list
+ * Return: number of items in list
+ */
+size_t print_listint(const list_t *node)
+{
+	int i = 0;
+
+	while (node)
+	{
+		if (!node->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", node->len, node->str);
+		++i;
+		node = (*node).next;
+	}
+
+	return (i);
+}
