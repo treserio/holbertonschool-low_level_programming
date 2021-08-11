7786219a45161d4ce8a334279dace069ffabc398
treserio <treserio@hotmail.com>
2021-08-11 12:29:29 -0500

    3 init

---
+++ 0x14-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+/**
+ * add_nodeint - add a node to the front of the list
+ * @head: address of first node pointer
+ * @n: int value for new node
+ * Return: NULL if fail, or ptr to first item of list
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new = malloc(sizeof(listint_t));
+	int i;
+
+	if (!new || !head || !n)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	new->n = n;
+	new->next = NULL;
+
+	/* find the tail of head */
+	while (*head)
+	{
+		++i;
+		head = (*head)->next;
+	}
+
+	*head = new;
+
+	return (*head);
+}
