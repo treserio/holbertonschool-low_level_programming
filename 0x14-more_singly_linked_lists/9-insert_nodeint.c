4a600bbf8d8ada03d299fde8798bc02a4ca39803
treserio <treserio@hotmail.com>
2021-08-12 00:00:15 -0500

    9.5 moved head if idx = 0

--- 0x14-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x14-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,6 +23,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		new->n = n;
 		new->next = *head;
+		*head = new;
 		return (new);
 	}
 	/* else start after first node */
