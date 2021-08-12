61bf237fa6a3cb7849ae2707f437da2b551e5bd8
treserio <treserio@hotmail.com>
2021-08-11 23:53:32 -0500

    10 init

--- 0x14-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x14-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,65 +1,38 @@
 #include "lists.h"
-#include "3-add_nodeint_end.c"
-#include "0-print_listint.c"
-#include "5-free_listint2.c"
-
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 /**
- * delete_nodeint_at_index - free all elements of a list
- * @head: starting node in list
- * @index: value to remove
- * Return: 1 if succeed 2 if fail
+ * delete_nodeint_at_index - deletes a node at the index.
+ * @head: the list.
+ * @index: where the node needs to be deleted.
+ * Return: 1 if succeeded, 0 if not.
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
+	listint_t *node, *delete = (listint_t *)*head;
 	unsigned int i;
-	listint_t *tmp;
 
-	for (i = 0; head && *head && i <= index; ++i, *head = (*head)->next)
+	if (*head == NULL)
 	{
-		if (i == 0)
-		{
-			tmp = *head;
-			head = &(*head)->next;
-			free(tmp);
-		}
-		else if (i == index - 1)
-			tmp = (*head);
+		return (-1);
 	}
-	tmp->next = (*head)->next;
-	free(head);
-
-	return ((*head)->n);
-}
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    listint_t *head;
-    int n;
-
-    head = NULL;
-    add_nodeint_end(&head, 0);
-    add_nodeint_end(&head, 1);
-    add_nodeint_end(&head, 2);
-    add_nodeint_end(&head, 3);
-    add_nodeint_end(&head, 4);
-    add_nodeint_end(&head, 98);
-    add_nodeint_end(&head, 402);
-    add_nodeint_end(&head, 1024);
-    print_listint(head);
-    n = pop_listint(&head);
-    printf("- %d\n", n);
-    print_listint(head);
-    n = pop_listint(&head);
-    printf("- %d\n", n);
-    print_listint(head);
-    free_listint2(&head);
-    printf("%p\n", (void *)head);
-    return (0);
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(delete);
+		return (1);
+	}
+	for (i = 0; i < index; i++)
+	{
+		if (i + 1 == index)
+		{
+			node = delete;
+		}
+		delete = delete->next;
+		if (delete == NULL)
+		{
+			return (-1);
+		}
+	}
+	node->next = delete->next;
+	free(delete);
+	return (1);
 }
