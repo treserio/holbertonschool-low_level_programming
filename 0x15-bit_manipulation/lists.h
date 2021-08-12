4922787e8920c1b28c6f20eedd8be85f7faaa040
Tres Serio <treserio@hotmail.com>
2021-08-12 14:10:34 -0700

    +hdr

---
+++ 0x15-bit_manipulation/lists.h
@@ -0,0 +1,15 @@
+#ifndef _More_L_Lists_h
+#define _More_L_Lists_h
+
+#include <stdlib.h>
+#include <stdio.h>
+
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
+
+#endif
