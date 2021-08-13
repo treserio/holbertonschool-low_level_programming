93e8638d8dd74022aeda1040e1fc8be61820affa
treserio <treserio@hotmail.com>
2021-08-12 22:05:13 -0500

    2 init

---
+++ 0x15-bit_manipulation/2-get_bit.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * get_bit - gets a bit at a certain index.
+ * @val: the value to examine the bit
+ * @index: the index of the bit to check
+ * Return: the bit set at index of val or -1 if failed.
+ */
+
+int get_bit(unsigned long int val, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+	val = val >> index;
+	return (1 & val);
+}
