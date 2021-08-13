eb7e96aee017cfdf46071eef0fffd3e7d408c640
treserio <treserio@hotmail.com>
2021-08-12 23:37:18 -0500

    5.1 testing

--- 0x15-bit_manipulation/5-flip_bits.c
+++ 0x15-bit_manipulation/5-flip_bits.c
@@ -8,22 +8,12 @@
 unsigned int flip_bits(unsigned long int num1, unsigned long int num2)
 {
 	int count = 0;
+	unsigned long int dif = num1^num2;
 
-	if num1 > num2;
+	for (; dif > 0; --dif)
 	{
-		for (; num1 > 0; num1 >> 1, num2 >> 1)
-		{
-			if (&num1^&num2 == 1)
-				++count;
-		}
+		count += (1 & dif);
 	}
-	else
-	{
-		for (; num2 > 0; num2 >> 1, num1 >> 1)
-		{
-			if (&num1^&num2 == 1)
-				++count;
-		}
-	}
+
 	return (count);
 }
