42e3aa1768ca8d82fc734b71efe60153817b2d18
Tres Serio <treserio@hotmail.com>
2021-08-17 13:44:56 -0700

    2.1 bettied

--- 0x16-file_io/2-append_text_to_file.c
+++ 0x16-file_io/2-append_text_to_file.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 /**
- * append_text_to_file - read "letters" amount of text from a file
- * @filename: the name of the file to read
- * @letters: the number of chars to read from the file
- * Return: 0 on failure, else the number of chars printed to stdout
+ * append_text_to_file - append text to the specified file
+ * @filename: the name of the file to append text to
+ * @input: the string to append to the file
+ * Return: 1 on success, else -1
  */
 int append_text_to_file(const char *filename, char *input)
 {
@@ -11,18 +11,18 @@ int append_text_to_file(const char *filename, char *input)
 
 	if (!filename)
 		return (-1);
-	
+
 	if (!input)
 		input = "";
-	
-	file = open(filename, O_APPEND)
+
+	file = open(filename, O_RDWR | O_APPEND);
 	if (file == -1)
 		return (-1);
-	
+
 	chk = write(file, input, strlen(input));
-	if (chk == -1 || chk == 13)
+	if (chk == -1)
 		return (-1);
-	
+
 	close(file);
 	return (1);
 }
