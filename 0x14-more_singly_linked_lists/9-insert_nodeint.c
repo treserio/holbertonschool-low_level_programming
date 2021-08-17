df3498cd7ce7f79952809faf772b790b91d0f43b
Tres Serio <treserio@hotmail.com>
2021-08-16 19:09:24 -0700

    9 bettied again, space ghost

--- 0x14-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x14-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,7 +17,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	/* check for zero idx, meaning before list */
 	if (!idx)
-	{ 
+	{
 		new->n = n;
 		new->next = *head;
 		*head = new;
