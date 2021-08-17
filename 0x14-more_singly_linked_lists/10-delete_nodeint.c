c98d7619867511925673ee4494ef29661561fd53
treserio <treserio@hotmail.com>
2021-08-16 21:43:12 -0500

    0x14 10 rework

--- 0x14-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x14-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,38 +1,104 @@
 #include "lists.h"
+#include "3-add_nodeint_end.c"
+#include "0-print_listint.c"
+#include "5-free_listint2.c"
+#include "6-pop_listint.c"
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
 /**
- * delete_nodeint_at_index - deletes a node at the index.
- * @head: the list.
- * @index: where the node needs to be deleted.
- * Return: 1 if succeeded, 0 if not.
+ * delete_nodeint_at_index - delete a node at a specific index
+ * @head: starting node in list
+ * @index: note to remove
+ * Return: 1 if succeed -1 if fail
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *node, *delete = (listint_t *)*head;
 	unsigned int i;
+	listint_t *tmp = *head, *fixer;
 
-	if (*head == NULL)
-	{
+	if(!head || !*head)
 		return (-1);
-	}
 	if (index == 0)
 	{
 		*head = (*head)->next;
-		free(delete);
+		free(tmp);
 		return (1);
 	}
-	for (i = 0; i < index; i++)
+	/* iterate to the indexed node */
+	for (i = 0; tmp && i < index; ++i, tmp = tmp->next)
+		fixer = tmp;
+
+	if (i == index)
 	{
-		if (i + 1 == index)
-		{
-			node = delete;
-		}
-		delete = delete->next;
-		if (delete == NULL)
-		{
-			return (-1);
-		}
+		fixer->next = tmp->next;
+		free(tmp);
+		return (1);
 	}
-	node->next = delete->next;
-	free(delete);
-	return (1);
+	return (-1);
+}
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    listint_t *head;
+
+    head = NULL;
+    add_nodeint_end(&head, 0);
+    add_nodeint_end(&head, 1);
+    add_nodeint_end(&head, 2);
+    add_nodeint_end(&head, 3);
+    add_nodeint_end(&head, 4);
+    add_nodeint_end(&head, 98);
+    add_nodeint_end(&head, 402);
+    add_nodeint_end(&head, 1024);
+    print_listint(head);
+    printf("-----------------\n");
+    delete_nodeint_at_index(&head, 5);
+    print_listint(head);
+    printf("-----------------\n");
+    delete_nodeint_at_index(&head, 0);
+    print_listint(head);
+    printf("-----------------\n");
+    delete_nodeint_at_index(&head, 0);
+    print_listint(head);
+    printf("-----------------\n");
+    delete_nodeint_at_index(&head, 0);
+    print_listint(head);
+    printf("-----------------\n");
+    delete_nodeint_at_index(&head, 0);
+    print_listint(head);
+    printf("-----------------\n");
+    delete_nodeint_at_index(&head, 0);
+    print_listint(head);
+    printf("-----------------\n");
+    delete_nodeint_at_index(&head, 0);
+    print_listint(head);
+    printf("-----------------\n");
+    delete_nodeint_at_index(&head, 0);
+    printf("-----------------\n");
+    delete_nodeint_at_index(&head, 0);
+    printf("-----------------\n");
+    delete_nodeint_at_index(&head, 0);
+    printf("-----------------\n");
+    delete_nodeint_at_index(&head, 0);
+    printf("-----------------\n");
+    delete_nodeint_at_index(&head, 0);
+    printf("-----------------\n");
+    delete_nodeint_at_index(&head, 0);
+    printf("-----------------\n");
+    delete_nodeint_at_index(&head, 0);
+    printf("-----------------\n");
+    delete_nodeint_at_index(&head, 0);
+    printf("-----------------\n");
+    delete_nodeint_at_index(&head, 0);
+    printf("-----------------\n");
+    delete_nodeint_at_index(&head, 0);
+    print_listint(head);
+    return (0);
 }
