00d1fcf8e8107e4a5865e707415046b8ea07752e
Tres Serio <treserio@hotmail.com>
2021-08-17 17:47:48 -0700

    3.7

--- 0x16-file_io/3-cp.c
+++ 0x16-file_io/3-cp.c
@@ -26,8 +26,8 @@ int main(int argc, char *argv[])
 	while ((chk = read(f_frm, temp, 1024)) != 0)
 	{
 		if (chk == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
-		exit(98);			
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
+			exit(98);
 		if (write(f_to, temp, chk) != chk)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
