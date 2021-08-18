e57f002a7848e8a814f9cff2db3f6f2a6dfe0cc1
Tres Serio <treserio@hotmail.com>
2021-08-18 07:52:19 -0700

    0x16 0.2 cmplr

--- 0x16-file_io/0-read_textfile.c
+++ 0x16-file_io/0-read_textfile.c
@@ -13,7 +13,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (!filename || !temp)
 		return (0);
 
-	open(filename, O_RDONLY);
+	chk = open(filename, O_RDONLY);
 	if (chk == -1)
 		return (0);
 
