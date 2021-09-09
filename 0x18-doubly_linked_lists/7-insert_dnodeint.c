442f0a74b56b03e6036ad1161d4436f6ce865cc1
treserio <treserio@hotmail.com>
2021-09-08 22:07:01 -0500

    7.3 quit finding real head

--- 0x18-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x18-doubly_linked_lists/7-insert_dnodeint.c
@@ -17,9 +17,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (!h)
 		return (NULL);
 	start = *h;
-	/* confirm we're at the top */
-	for (; h && *h && (*h)->prev; *h = (*h)->prev)
-	;
 	/* find the idxed node */
 	for (; h && i < (idx - 1); *h = (*h)->next, ++i)
 	;
