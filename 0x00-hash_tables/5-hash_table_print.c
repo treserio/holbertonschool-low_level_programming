c509bd95392d7c6063a9b026dc986928f49cb461
Tres Serio <treserio@hotmail.com>
2022-02-17 18:40:14 -0600

    5 init

---
+++ 0x00-hash_tables/5-hash_table_print.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - print the values of a hash_table in order
+ * @ht: the hash table to insert into
+ * Return: void
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i, comma;
+
+	if (!ht || !ht->array)
+		return;
+	printf("{");
+	for (i = 0, comma = 0; i < ht->size; ++i)
+	{
+		if (comma && ht->array[i])
+			printf(", ");
+		if (ht->array[i])
+			printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value), ++comma;
+	}
+	printf("}\n");
+}
