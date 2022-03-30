9763e9641f0afa39cd973d1f81ad274844f21553
Tres Serio <treserio@hotmail.com>
2022-03-30 18:06:59 -0500

    105 init, minor adjustments from 106

---
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -0,0 +1,49 @@
+#include "search_algos.h"
+/**
+ * jump_list - search through sorted list using skip algorithm
+ * @list: the head of the skip list to search
+ * @size: the total number of nodes in the list
+ * @value: the number to search for
+ * Return: the node where the value was found
+ */
+listint_t *jump_list(listint_t *list, size_t size, int value)
+{
+	listint_t *run, *jumper;
+	size_t cnt, exp, jump;
+	/* initial check for list */
+	if (!list)
+		return (NULL);
+	for (run = list, cnt = 0; run; run = run->next, ++cnt)
+	;
+	/* get our express value and total nodes, only use for total is in printing */
+	exp = sqrt((double)size);
+	for (run = list, jumper = list; 1; jumper = run)
+	{
+		if (run->index)
+			printf("Value checked at index [%ld] = [%d]\n", run->index, run->n);
+		/* jump if the value is lower */
+		if (run->n < value)
+		{
+			jump = run->index;
+			for (cnt = 0; cnt < exp && run; run = run->next, ++cnt)
+			;
+		}
+		/* if we've exceeded the value or we've hit the end of the list */
+		if ((run && run->n >= value) || !run)
+			break;
+	}
+	if (run)
+		printf("Value checked at index [%ld] = [%d]\n", run->index, run->n);
+	/* fix final index printing if we're close */
+	if (size - 1 < jump + exp)
+		printf("Value found between indexes [%ld] and [%ld]\n", jump, size - 1);
+	else
+		printf("Value found between indexes [%ld] and [%ld]\n", jump, jump + exp);
+	/* search within range for value */
+	for (run = jumper; run && run->n != value; run = run->next)
+		printf("Value checked at index [%ld] = [%d]\n", run->index, run->n);
+	/* checker still wants print to show when value is found */
+	if (run)
+		printf("Value checked at index [%ld] = [%d]\n", run->index, run->n);
+	return (run);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -23,6 +23,22 @@ typedef struct skiplist_s
     struct skiplist_s *express;
 } skiplist_t;
 
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+    int n;
+    size_t index;
+    struct listint_s *next;
+} listint_t;
+
 /* linear search functions */
 int linear_search(int *array, size_t size, int value);
 
@@ -33,6 +49,7 @@ int advanced_binary(int *array, size_t size, int value);
 
 /* linear_skip */
 skiplist_t *linear_skip(skiplist_t *list, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
 
 /* helpers */
 void p_array(int *array, size_t idx, size_t end);
